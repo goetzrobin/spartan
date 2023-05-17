22983fd45dc2620daff39df6d4d8666ebadb6071
Robin Goetz <tug29225@temple.edu>
2023-05-17 11:00:13 -0400

    feat: add alert to page

--- apps/analog-trpc/src/app/pages/index.page.ts
+++ apps/analog-trpc/src/app/pages/index.page.ts
@@ -40,6 +40,12 @@ import {
   HlmAccordionItemDirective,
   HlmAccordionTriggerDirective
 } from '@ng-spartan/accordion/helm';
+import {
+  HlmAlertDescriptionDirective,
+  HlmAlertDirective,
+  HlmAlertIconDirective,
+  HlmAlertTitleDirective
+} from '@ng-spartan/alert/helm';
 
 @Component({
   selector: 'analog-trpc-home',
@@ -63,7 +69,8 @@ import {
     HlmInputDirective,
     HlmCardDirective, HlmCardHeaderDirective, HlmCardTitleDirective, HlmCardContentDirective, HlmCardFooterDirective, HlmCardDescriptionDirective,
 
-    BrnAccordionComponent, BrnAccordionItemComponent, BrnAccordionTriggerComponent, BrnAccordionContentComponent, HlmAccordionDirective, HlmAccordionItemDirective, HlmAccordionContentDirective, HlmAccordionTriggerDirective, HlmAccordionIconComponent
+    BrnAccordionComponent, BrnAccordionItemComponent, BrnAccordionTriggerComponent, BrnAccordionContentComponent, HlmAccordionDirective, HlmAccordionItemDirective, HlmAccordionContentDirective, HlmAccordionTriggerDirective, HlmAccordionIconComponent,
+    HlmAlertDirective, HlmAlertIconDirective, HlmAlertTitleDirective, HlmAlertDescriptionDirective
   ],
   providers: [withErrorComponent(InputErrorComponent)],
   host: {
@@ -167,21 +174,26 @@ import {
         </brn-accordion-content>
       </brn-accordion-item>
 
-      <brn-accordion-item hlm>
-        <brn-accordion-trigger hlm>
-          <span>Introducing <span class='font-semibold italic text-[#DD0031]'>SPARTAN</span> helm & brain</span>
-          <hlm-accordion-icon />
-        </brn-accordion-trigger>
-        <brn-accordion-content hlm>
-          The components used on this page are also the intiial building blocks of a new UI library.
+
+      <div class='mt-12' hlmAlert>
+        <svg hlmAlertIcon class='h-4 w-4' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'
+             stroke-width='1.5'
+             stroke='currentColor'>
+          <path stroke-linecap='round' stroke-linejoin='round'
+                d='M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z' />
+        </svg>
+
+        <h4 hlmAlertTitle>Introducing <span class='font-semibold italic text-[#DD0031]'>SPARTAN</span> helm & brain</h4>
+        <p hlmAlertDesc> The components used on this page are also the intiial building blocks of a new UI library.
           It is made up of headless UI providers, the brain components/directives, which add ARIA compliant markup and
           interactions.
-          On top of the brain we add helm(et) directives, which add <a hlmBtn variant='link' class='h-6 px-0.5'
-                                                                       href='https://ui.shadcn.com' target='_blank'>shadcn</a>-like
-          styles to our application.
-        </brn-accordion-content>
-      </brn-accordion-item>
-    </brn-accordion>
+          On top of the brain we add helm(et) directives, which add
+          <a hlmBtn variant='link' class='h-6 px-0.5'
+             href='https://ui.shadcn.com'
+             target='_blank'>shadcn</a>-like
+          styles to our application.</p>
+
+      </div>
   `
 })
 export default class HomeComponent {
