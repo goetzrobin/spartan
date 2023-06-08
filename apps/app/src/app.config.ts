276ab7809b010667e5249fbd28c2954b2d6f1e80
Robin Goetz <tug29225@temple.edu>
2023-06-08 15:53:21 -0400

    feat: add navigationerrorhandler to config

--- apps/app/src/app.config.ts
+++ apps/app/src/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 import { provideTRPCClient } from './trpc-client';
+import { withNavigationErrorHandler } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideFileRouter(), provideClientHydration(), provideTRPCClient()],
+  providers: [provideFileRouter(withNavigationErrorHandler(console.log)), provideClientHydration(), provideTRPCClient()]
 };
