0a6a2dc5b42bda251f29036db962a418a3bdc927
Miha Mulec <miha.mulec@marand.si>
2023-06-16 13:36:48 +0200

    fix: revert app.component to main

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HlmAspectRatioDirective } from '@ng-spartan/ui/aspect-ratio/helm';
 import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'analog-trpc-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HlmAspectRatioDirective],
+  imports: [RouterOutlet, HeaderComponent],
   host: {
     class: 'max-w-screen-lg mx-auto text-foreground block h-full',
   },
   template: `
     <analog-trpc-header />
     <router-outlet></router-outlet>
-    <div hlmAspectRatio="16/10">
-      <div style="background: url('https://picsum.photos/seed/picsum/800/600'); background-size: cover"></div>
-    </div>
   `,
 })
 export class AppComponent {}
