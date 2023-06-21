3fdaa8bc1204c236af8cb82dd392263eb9f32448
Robin Goetz <tug29225@temple.edu>
2023-06-21 17:04:26 -0400

    feat: add skeleton stories

---
+++ libs/ui/skeleton/skeleton.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { HlmSkeletonComponent } from './helm/src';
+
+const meta: Meta<{}> = {
+  title: 'Skeleton',
+  decorators: [
+    moduleMetadata({
+      imports: [HlmSkeletonComponent],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<{}>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+   <div class='border p-4 m-4 w-fit rounded-lg border-border flex items-center space-x-4'>
+      <hlm-skeleton class='h-12 w-12 rounded-full' />
+      <div class='space-y-2'>
+        <hlm-skeleton class='h-4 w-[250px]' />
+        <hlm-skeleton class='h-4 w-[200px]' />
+      </div>
+    </div>
+    `,
+  }),
+};
