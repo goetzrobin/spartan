d943aa96778e89cf6faadc0968cde5ec230da309
robingotz <tug29225@temple.edu>
2023-06-28 10:39:15 -0400

    feat: add tailwind story for icon

--- libs/ui/icon/icon.stories.ts
+++ libs/ui/icon/icon.stories.ts
@@ -1,6 +1,6 @@
 import { provideIcons } from '@ng-icons/core';
 import * as radixIcons from '@ng-icons/radix-icons';
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { HlmIconComponent } from './helm/src';
 
 type IconProps = {
@@ -30,11 +30,23 @@ export const Default: Story = {
   args: {
     name: 'radixCheck',
     size: '2rem',
-    color: 'black',
+    color: 'red',
     strokeWidth: 1,
   },
   argTypes: {
     name: { control: 'select', options: Object.keys(radixIcons) },
     color: { control: 'color' },
+  },
+};
+
+export const Tailwind: Story = {
+  render: ({ name }) => ({
+    template: `<hlm-icon name='${name}' class='text-red-600 text-5xl' />`,
+  }),
+  args: {
+    name: 'radixCheck',
+  },
+  argTypes: {
+    name: { control: 'select', options: Object.keys(radixIcons) },
   },
 };
