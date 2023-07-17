1beb224f23c272da7c4da7aa38212dd091e79b31
robingotz <tug29225@temple.edu>
2023-07-17 09:57:36 -0400

    fix: fix unit test for avatar image by adding new class

--- libs/ui/avatar/helm/src/lib/image/hlm-avatar-image.directive.spec.ts
+++ libs/ui/avatar/helm/src/lib/image/hlm-avatar-image.directive.spec.ts
@@ -27,7 +27,7 @@ describe('HlmAvatarImageDirective', () => {
 
   it('should add the default classes if no inputs are provided', () => {
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('img').className).toBe('aspect-square h-full w-full');
+    expect(fixture.nativeElement.querySelector('img').className).toBe('aspect-square h-full object-cover w-full');
   });
 
   it('should add any user defined classes', async () => {
