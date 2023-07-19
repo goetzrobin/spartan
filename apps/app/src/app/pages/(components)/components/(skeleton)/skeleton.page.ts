81400c7b76f90a74bb2b129f8dba8bb4d6fa1c8f
robingotz <tug29225@temple.edu>
2023-07-19 13:11:41 -0400

    feat: add skeleton docs

---
+++ apps/app/src/app/pages/(components)/components/(skeleton)/skeleton.page.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { RouteMeta } from '@analogjs/router';
+import { metaWith } from '~/app/shared/meta/meta.util';
+import { CodeComponent } from '~/app/shared/code/code.component';
+import { CodePreviewDirective } from '~/app/shared/code/code-preview.directive';
+import { MainSectionDirective } from '~/app/shared/layout/main-section.directive';
+import { PageBottomNavComponent } from '~/app/shared/layout/page-bottom-nav/page-bottom-nav.component';
+import { PageBottomNavLinkComponent } from '~/app/shared/layout/page-bottom-nav/page-bottom-nav-link.component';
+import { PageNavComponent } from '~/app/shared/layout/page-nav/page-nav.component';
+import { PageNavLinkComponent } from '~/app/shared/layout/page-nav/page-nav-link.component';
+import { SectionIntroComponent } from '~/app/shared/layout/section-intro.component';
+import { SectionSubHeadingComponent } from '~/app/shared/layout/section-sub-heading.component';
+import { TabsComponent } from '~/app/shared/layout/tabs.component';
+import { defaultCode, defaultImports, defaultSkeleton, SkeletonPreviewComponent } from './skeleton.preview';
+
+export const routeMeta: RouteMeta = {
+  data: { breadcrumb: 'Skeleton' },
+  meta: metaWith('spartan/ui - Skeleton', 'Use to show a placeholder while content is loading.'),
+  title: 'spartan/ui - Skeleton',
+};
+@Component({
+  selector: 'spartan-skeleton',
+  standalone: true,
+  imports: [
+    MainSectionDirective,
+    CodeComponent,
+    SectionIntroComponent,
+    SectionSubHeadingComponent,
+    TabsComponent,
+    CodePreviewDirective,
+    PageNavLinkComponent,
+    PageNavComponent,
+    PageBottomNavComponent,
+    PageBottomNavLinkComponent,
+    SkeletonPreviewComponent,
+  ],
+  template: `
+    <section spartanMainSection>
+      <spartan-section-intro name="Skeleton" lead="Use to show a placeholder while content is loading." />
+
+      <spartan-tabs firstTab="Preview" secondTab="Code">
+        <div spartanCodePreview firstTab>
+          <spartan-skeleton-preview />
+        </div>
+        <spartan-code secondTab [code]="defaultCode" />
+      </spartan-tabs>
+
+      <spartan-section-sub-heading id="installation">Installation</spartan-section-sub-heading>
+      <spartan-tabs class="mt-4" firstTab="yarn" secondTab="npm">
+        <spartan-code firstTab language="sh" code="yarn install @spartan.ng/ui/skeleton" />
+        <spartan-code secondTab language="sh" code="npm install @spartan.ng/ui/skeleton" />
+      </spartan-tabs>
+
+      <spartan-section-sub-heading id="usage">Usage</spartan-section-sub-heading>
+      <div class="space-y-4">
+        <spartan-code [code]="defaultImports" />
+        <spartan-code [code]="defaultSkeleton" />
+      </div>
+
+      <spartan-page-bottom-nav>
+        <spartan-page-bottom-nav-link href="switch" label="Switch" />
+        <spartan-page-bottom-nav-link direction="previous" href="sheet" label="Sheet" />
+      </spartan-page-bottom-nav>
+    </section>
+    <spartan-page-nav>
+      <spartan-page-nav-link fragment="installation" label="Installation" />
+      <spartan-page-nav-link fragment="usage" label="Usage" />
+    </spartan-page-nav>
+  `,
+})
+export default class SkeletonPageComponent {
+  protected readonly defaultCode = defaultCode;
+  protected readonly defaultSkeleton = defaultSkeleton;
+  protected readonly defaultImports = defaultImports;
+}
