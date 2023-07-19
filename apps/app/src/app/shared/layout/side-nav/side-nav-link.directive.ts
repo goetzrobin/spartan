7c5e54812ec4385470fc8239511505c21b61ea3e
robingotz <tug29225@temple.edu>
2023-07-19 11:23:18 -0400

    fix: make text less prominent when link disabled

--- apps/app/src/app/shared/layout/side-nav/side-nav-link.directive.ts
+++ apps/app/src/app/shared/layout/side-nav/side-nav-link.directive.ts
@@ -14,7 +14,8 @@ import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
   ],
   host: {
     '[tabindex]': '_disabled ? "-1" : "0"',
-    '[class.text-gray-300]': '_disabled',
+    '[class.!text-zinc-300]': '_disabled',
+    '[class.dark:!text-zinc-700]': '_disabled',
     '[class.pointer-events-none]': '_disabled',
     class:
       'group relative flex w-full justify-between items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
