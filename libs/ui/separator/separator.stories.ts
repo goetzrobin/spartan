9ccfb68e151a59efe2d494fd9928e22ce1434423
robingotz <tug29225@temple.edu>
2023-07-19 13:04:25 -0400

    refactor(separator): reformat story

--- libs/ui/separator/separator.stories.ts
+++ libs/ui/separator/separator.stories.ts
@@ -17,21 +17,22 @@ type Story = StoryObj<{}>;
 export const Default: Story = {
   render: () => ({
     template: `
- <div>
-      <div class="space-y-1">
-        <h4 class="text-sm font-medium leading-none">Radix Primitives</h4>
-        <p class="text-sm text-muted-foreground">
+    <div>
+      <div class='space-y-1'>
+        <h4 class='text-sm font-medium leading-none'>Radix Primitives</h4>
+        <p class='text-sm text-muted-foreground'>
           An open-source UI component library.
         </p>
       </div>
-      <brn-separator hlmSeparator class="my-4"/>
-      <div class="flex h-5 items-center space-x-4 text-sm">
+      <brn-separator hlmSeparator class='my-4' />
+      <div class='flex h-5 items-center space-x-4 text-sm'>
         <div>Blog</div>
-        <brn-separator decorative hlmSeparator orientation="vertical"/>
+        <brn-separator decorative hlmSeparator orientation='vertical' />
         <div>Docs</div>
-        <brn-separator decorative hlmSeparator orientation="vertical"/>
+        <brn-separator decorative hlmSeparator orientation='vertical' />
         <div>Source</div>
       </div>
-    </div>    `,
+    </div>
+       `,
   }),
 };
