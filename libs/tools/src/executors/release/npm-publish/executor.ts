d93e6527ea51a694f114b7449d7ee745712206b2
robingotz <tug29225@temple.edu>
2023-07-24 10:21:13 -0400

    build: set up verdaccio and test with alpha version
    move from nx to tools so we can use nx publicly.
    using 10 because I used 1 already on npm and it complains.
    learning a lot as I go here.

--- libs/tools/src/executors/release/npm-publish/executor.ts
+++ libs/tools/src/executors/release/npm-publish/executor.ts
@@ -1,16 +1,13 @@
 import { execSync } from 'child_process';
 import { ExecutorContext } from '@nx/devkit';
 
-import { getRoot, sleep } from '../helpers/projects.helpers';
+import { getRoot } from '../helpers/projects.helpers';
 
 import { NpmPublishExecutorSchema } from './schema';
 
 export default async function runExecutor(options: NpmPublishExecutorSchema, context: ExecutorContext) {
   const sourceRoot = `./dist/${getRoot(context)}`;
-  execSync(
-    `cd ${sourceRoot} && echo '//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}' > .npmrc && npm publish`
-  );
-  await sleep(3000);
+  execSync(`cd ${sourceRoot} && npm publish ${process.env.TAG ? '--tag ' + process.env.TAG : ''}`);
   return {
     success: true,
   };
