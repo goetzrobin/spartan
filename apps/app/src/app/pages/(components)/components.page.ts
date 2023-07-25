fdf0a87e4173dea12b40840bb9230684cd758223
robingotz <tug29225@temple.edu>
2023-07-25 12:22:10 -0400

    feat: add disclaimer to components page

--- apps/app/src/app/pages/(components)/components.page.ts
+++ apps/app/src/app/pages/(components)/components.page.ts
@@ -2,6 +2,15 @@ import { RouteMeta } from '@analogjs/router';
 import { metaWith } from '~/app/shared/meta/meta.util';
 import { Component } from '@angular/core';
 import { PageComponent } from '~/app/shared/layout/page.component';
+import {
+  HlmAlertDescriptionDirective,
+  HlmAlertDirective,
+  HlmAlertIconDirective,
+  HlmAlertTitleDirective,
+} from '@spartan-ng/ui-alert-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { provideIcons } from '@ng-icons/core';
+import { radixRocket } from '@ng-icons/radix-icons';
 
 export const routeMeta: RouteMeta = {
   meta: metaWith(
@@ -17,7 +26,26 @@ export const routeMeta: RouteMeta = {
 @Component({
   selector: 'spartan-components',
   standalone: true,
-  imports: [PageComponent],
-  template: `<spartan-page /> `,
+  imports: [
+    PageComponent,
+    HlmAlertDirective,
+    HlmAlertTitleDirective,
+    HlmAlertDescriptionDirective,
+    HlmIconComponent,
+    HlmAlertIconDirective,
+  ],
+  providers: [provideIcons({ radixRocket })],
+  template: `
+    <div hlmAlert class="m-2 p-4 rounded-lg text-destructive-foreground border border-border bg-destructive">
+      <hlm-icon hlmAlertIcon name="radixRocket" class="!text-destructive-foreground" />
+      <h2 hlmAlertTitle>Components are coming soon...</h2>
+      <p hlmAlertDesc>
+        We are actively working on publishing these components. They will soon be released.
+        <a class="underline" target="_blank" href="https://github.com/goetzrobin/spartan">Star us on GitHub</a> and
+        follow along as we build the next Angular UI library. Check out the preview below to see what they'll look like:
+      </p>
+    </div>
+    <spartan-page />
+  `,
 })
 export default class ComponentsPageComponent {}
