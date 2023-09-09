245b3cdf5771879468735495b1634deaee4296be
robingotz <tug29225@temple.edu>
2023-09-09 14:12:14 +0200

    feat: fix three hundred font size

--- apps/app/src/app/pages/(home)/components/th-item.component.ts
+++ apps/app/src/app/pages/(home)/components/th-item.component.ts
@@ -9,7 +9,7 @@ import { SpartanLogoComponent } from '~/app/shared/spartan-logo.component';
     class: 'inline-flex flex-col justify-center items-center',
   },
   template: `<spartan-logo class="-rotate-90 h-8 w-8 rounded-full bg-primary [&>svg]:text-primary-foreground p-1" /><a
-      class="hover:underline inline-block text-sm mt-1 font-medium"
+      class="hover:underline whitespace-nowrap inline-block text-[.55rem] mt-1 font-medium"
       [href]="href"
       target="_blank"
       ><ng-content
