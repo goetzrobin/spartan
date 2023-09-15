24f167f219b0370a2241576f038bfdd4b252f634
robingotz <tug29225@temple.edu>
2023-09-15 17:23:01 +0200

    feat: expose disabled/checked through directive
    also cleaning up some of the code to use the underlying directives in a better way

--- libs/ui/menu/brain/src/lib/brn-menu-item-checkbox.directive.ts
+++ libs/ui/menu/brain/src/lib/brn-menu-item-checkbox.directive.ts
@@ -1,33 +1,31 @@
-import { Directive, EventEmitter, HostBinding, inject, Input, Output } from '@angular/core';
+import { Directive, HostBinding, inject, Input, Output } from '@angular/core';
 import { CdkMenuItemCheckbox } from '@angular/cdk/menu';
 import { BooleanInput } from '@angular/cdk/coercion';
 
 @Directive({
   selector: '[brnMenuItemCheckbox]',
   standalone: true,
-  hostDirectives: [
-    {
-      directive: CdkMenuItemCheckbox,
-      // eslint-disable-next-line @angular-eslint/no-outputs-metadata-property
-      outputs:
-        // eslint-disable-next-line @angular-eslint/no-output-rename
-        ['cdkMenuItemTriggered: triggered'],
-    },
-  ],
+  hostDirectives: [CdkMenuItemCheckbox],
 })
 export class BrnMenuItemCheckboxDirective {
-  private _cdkMenuItem = inject(CdkMenuItemCheckbox, { host: true });
+  private readonly _cdkMenuItem = inject(CdkMenuItemCheckbox, { host: true });
   @HostBinding('class.checked')
   private _checked = this._cdkMenuItem.checked;
-  @Input()
-  set disabled(value: BooleanInput) {
-    this._cdkMenuItem.disabled = value;
+  get checked() {
+    return this._checked;
   }
   @Input()
   set checked(value: BooleanInput) {
     this._cdkMenuItem.checked = value;
     this._checked = this._cdkMenuItem.checked;
   }
+  get disabled() {
+    return this._cdkMenuItem.disabled;
+  }
+  @Input()
+  set disabled(value: BooleanInput) {
+    this._cdkMenuItem.disabled = value;
+  }
   @Output()
-  triggered = new EventEmitter<void>();
+  triggered = this._cdkMenuItem.triggered;
 }
--- libs/ui/menu/brain/src/lib/brn-menu-item.directive.ts
+++ libs/ui/menu/brain/src/lib/brn-menu-item.directive.ts
@@ -1,26 +1,24 @@
-import { Directive, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Directive, inject, Input, Output } from '@angular/core';
 import { CdkMenuItem } from '@angular/cdk/menu';
 import { BooleanInput } from '@angular/cdk/coercion';
 
 @Directive({
   selector: '[brnMenuItem]',
   standalone: true,
-  hostDirectives: [
-    {
-      directive: CdkMenuItem,
-      // eslint-disable-next-line @angular-eslint/no-outputs-metadata-property
-      outputs:
-        // eslint-disable-next-line @angular-eslint/no-output-rename
-        ['cdkMenuItemTriggered: triggered'],
-    },
-  ],
+  hostDirectives: [CdkMenuItem],
 })
 export class BrnMenuItemDirective {
   private _cdkMenuItem = inject(CdkMenuItem, { host: true });
+
+  get disabled() {
+    return this._cdkMenuItem.disabled;
+  }
+
   @Input()
   set disabled(value: BooleanInput) {
     this._cdkMenuItem.disabled = value;
   }
+
   @Output()
-  triggered = new EventEmitter<void>();
+  triggered = this._cdkMenuItem.triggered;
 }
