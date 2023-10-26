edf26629e3e900bd8fc3699d8f307946dd2b084d
Pascal Küsgen <pascalkuesgen@gmail.com>
2023-10-26 20:36:39 +0200

    docs: built index.mjs is inside server folder (#43)

--- apps/app/src/app/pages/(stack)/stack/installation.page.ts
+++ apps/app/src/app/pages/(stack)/stack/installation.page.ts
@@ -373,7 +373,7 @@ create table note (
         Finally, let's build a production bundle of our application. Run the command above. By default, AnalogJs will
         build a NodeJs-compatible output, which you can run with the following command:
       </p>
-      <spartan-code class="mt-3" language="sh" code="node dist/[YOUR_APP_NAME]/analog/index.mjs" />
+      <spartan-code class="mt-3" language="sh" code="node dist/[YOUR_APP_NAME]/analog/server/index.mjs" />
       <p class="${hlmP}">
         AnalogJs also supports multiple build presets, which makes it easy to deploy your application to most of the
         major cloud providers. This includes Vercel, Cloudflare, Azure, AWS, and more.
