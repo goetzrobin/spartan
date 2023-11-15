9f3cea0019e113442619e9f9b1ee0782a5176033
Andre Prilly Kurniawan <40825015+oidre@users.noreply.github.com>
2023-11-15 23:52:57 +0700

    docs: fix example code loading button (#50)

--- apps/app/src/app/pages/(components)/components/(button)/button--loading.example.ts
+++ apps/app/src/app/pages/(components)/components/(button)/button--loading.example.ts
@@ -19,12 +19,19 @@ export class ButtonLoadingComponent {}
 export const loadingCode = `
 import { Component } from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { provideIcons } from '@ng-icons/core';
+import { radixSymbol } from '@ng-icons/radix-icons';
 
 @Component({
-  selector: 'spartan-button-icon',
+  selector: 'spartan-button-loading',
   standalone: true,
-  imports: [HlmButtonDirective],
+  imports: [HlmButtonDirective, HlmSpinnerComponent, HlmIconComponent],
+  providers: [provideIcons({ radixSymbol })],
   template: \`
-    <button hlmBtn variant='icon'>Icon</button> \`,})
-export class ButtonIconComponent {}
+    <button disabled hlmBtn><hlm-icon name="radixSymbol" size="sm" class="mr-2 animate-spin" /> Please wait</button>
+  \`,
+})
+export class ButtonLoadingComponent {}
 `;
