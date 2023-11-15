1b3d2c89bed5b2b5c3d78547f08e3968025066ca
Andre <oidre.dev@gmail.com>
2023-11-16 00:22:34 +0700

    docs: adding gap between sidenav and main content

--- apps/app/src/app/shared/layout/container.directive.ts
+++ apps/app/src/app/shared/layout/container.directive.ts
@@ -7,5 +7,5 @@ import { Directive, HostBinding } from '@angular/core';
 export class ContainerDirective {
   @HostBinding('class')
   public class =
-    'w-full mx-auto px-1 flex flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)]';
+    'w-full mx-auto flex flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)] md:gap-6 lg:gap-10';
 }
