bb530635a1dcab0ceb41d81586bb8a5ccebecdae
elite-benni <benjamin.heiss@elite.tirol>
2023-11-24 23:34:27 +0100

    test(accordion): adjust e2e tests to directives

--- apps/ui-storybook-e2e/src/integration/accordion/accordion.cy.ts
+++ apps/ui-storybook-e2e/src/integration/accordion/accordion.cy.ts
@@ -1,8 +1,8 @@
 describe('accordion', () => {
 	const verifyAccordionSetup = () => {
-		cy.get('brn-accordion').should('have.attr', 'data-state', 'closed');
-		cy.get('brn-accordion-item').should('have.length', 3);
-		cy.get('brn-accordion-item').first().as('firstItem');
+		cy.get('[hlmaccordion]').should('have.attr', 'data-state', 'closed');
+		cy.get('[hlmAccordionItem]').should('have.length', 3);
+		cy.get('[hlmAccordionItem]').first().as('firstItem');
 		cy.get('@firstItem').next().as('secondItem');
 		cy.get('@secondItem').next().as('thirdItem');
 
@@ -11,7 +11,7 @@ describe('accordion', () => {
 		cy.get('@thirdItem').should('have.attr', 'data-state', 'closed');
 
 		cy.get('@firstItem')
-			.find('brn-accordion-trigger')
+			.find('[hlmAccordionTrigger]')
 			.should('have.attr', 'role', 'heading')
 			.should('have.id', 'brn-accordion-trigger-0')
 			.should('have.attr', 'data-state', 'closed')
@@ -24,17 +24,17 @@ describe('accordion', () => {
 	};
 
 	const verifyAccordionStateOpen = () => {
-		cy.get('brn-accordion').should('have.attr', 'data-state', 'open');
+		cy.get('[hlmaccordion]').should('have.attr', 'data-state', 'open');
 	};
 
 	const verifyAccordionStateClosed = () => {
-		cy.get('brn-accordion').should('have.attr', 'data-state', 'closed');
+		cy.get('[hlmaccordion]').should('have.attr', 'data-state', 'closed');
 	};
 
 	const verifyAccordionFirstItemStateOpen = () => {
 		cy.get('@firstItem').should('have.attr', 'data-state', 'open');
 		cy.get('@firstItem')
-			.find('brn-accordion-trigger')
+			.find('[hlmAccordionTrigger]')
 			.should('have.attr', 'data-state', 'open')
 			.should('have.attr', 'aria-expanded', 'true');
 		cy.get('@firstItem').find('brn-accordion-content').should('have.attr', 'data-state', 'open');
@@ -43,7 +43,7 @@ describe('accordion', () => {
 	const verifyAccordionFirstItemStateClosed = () => {
 		cy.get('@firstItem').should('have.attr', 'data-state', 'closed');
 		cy.get('@firstItem')
-			.find('brn-accordion-trigger')
+			.find('[hlmAccordionTrigger]')
 			.should('have.attr', 'data-state', 'closed')
 			.should('have.attr', 'aria-expanded', 'false');
 		cy.get('@firstItem').find('brn-accordion-content').should('have.attr', 'data-state', 'closed');
@@ -52,7 +52,7 @@ describe('accordion', () => {
 	const verifyAccordionSecondItemStateOpen = () => {
 		cy.get('@secondItem').should('have.attr', 'data-state', 'open');
 		cy.get('@secondItem')
-			.find('brn-accordion-trigger')
+			.find('[hlmAccordionTrigger]')
 			.should('have.attr', 'data-state', 'open')
 			.should('have.attr', 'aria-expanded', 'true');
 		cy.get('@secondItem').find('brn-accordion-content').should('have.attr', 'data-state', 'open');
@@ -61,7 +61,7 @@ describe('accordion', () => {
 	const verifyAccordionSecondItemStateClosed = () => {
 		cy.get('@secondItem').should('have.attr', 'data-state', 'closed');
 		cy.get('@secondItem')
-			.find('brn-accordion-trigger')
+			.find('[hlmAccordionTrigger]')
 			.should('have.attr', 'data-state', 'closed')
 			.should('have.attr', 'aria-expanded', 'false');
 		cy.get('@secondItem').find('brn-accordion-content').should('have.attr', 'data-state', 'closed');
@@ -77,12 +77,12 @@ describe('accordion', () => {
 			verifyAccordionSetup();
 
 			// click trigger first item to open it content
-			cy.get('@firstItem').find('brn-accordion-trigger').click();
+			cy.get('@firstItem').find('[hlmAccordionTrigger]').click();
 			verifyAccordionStateOpen();
 			verifyAccordionFirstItemStateOpen();
 
 			// click trigger first item to close it content
-			cy.get('@firstItem').find('brn-accordion-trigger').click();
+			cy.get('@firstItem').find('[hlmAccordionTrigger]').click();
 			verifyAccordionStateClosed();
 			verifyAccordionFirstItemStateClosed();
 		});
@@ -91,18 +91,18 @@ describe('accordion', () => {
 			verifyAccordionSetup();
 
 			// click trigger first item to open it content
-			cy.get('@firstItem').find('brn-accordion-trigger').click();
+			cy.get('@firstItem').find('[hlmAccordionTrigger]').click();
 			verifyAccordionStateOpen();
 			verifyAccordionFirstItemStateOpen();
 
 			// click trigger second item to open it content and close previous content
-			cy.get('@secondItem').find('brn-accordion-trigger').click();
+			cy.get('@secondItem').find('[hlmAccordionTrigger]').click();
 			verifyAccordionStateOpen();
 			verifyAccordionSecondItemStateOpen();
 			verifyAccordionFirstItemStateClosed();
 
 			// click trigger second item to close it content
-			cy.get('@secondItem').find('brn-accordion-trigger').click();
+			cy.get('@secondItem').find('[hlmAccordionTrigger]').click();
 			verifyAccordionStateClosed();
 			verifyAccordionSecondItemStateClosed();
 		});
