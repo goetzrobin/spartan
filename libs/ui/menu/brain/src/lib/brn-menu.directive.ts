0af36abb8bbd1a6609da10e044b0a7d8ddb5a7a4
robingotz <tug29225@temple.edu>
2023-12-01 16:45:56 -0500

    fix(dropdown-menu,context-menu,menubar): improve brn data attributes for menus
    Extend the BrnMenuDirective class to set the side and state
    attributes on the menu host. This update introduces new
    methods to determine the correct side where a submenu will
    appear and consequently set the attribute to the host. It
    also handles the menu's state when the host is about to
    close. A workaround was applied to approximate the
    appropriate submenu side.

--- libs/ui/menu/brain/src/lib/brn-menu.directive.ts
+++ libs/ui/menu/brain/src/lib/brn-menu.directive.ts
@@ -1,9 +1,42 @@
 import { CdkMenu } from '@angular/cdk/menu';
-import { Directive } from '@angular/core';
+import { Directive, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Directive({
 	selector: '[brnMenu],[brnSubMenu]',
 	standalone: true,
+	host: {
+		'[attr.data-state]': '_state()',
+		'[attr.data-side]': '_side()',
+	},
 	hostDirectives: [CdkMenu],
 })
-export class BrnMenuDirective {}
+export class BrnMenuDirective {
+	private readonly _host = inject(CdkMenu);
+
+	protected readonly _state = signal('open');
+	protected readonly _side = signal('top');
+
+	constructor() {
+		this.setSideWithDarkMagic();
+		// this is a best effort, but does not seem to work currently
+		// TODO: figure out a way for us to know the host is about to be closed. might not be possible with CDK
+		this._host.closed.pipe(takeUntilDestroyed()).subscribe(() => this._state.set('closed'));
+	}
+
+	private setSideWithDarkMagic() {
+		/**
+		 * This is an ugly workaround to at least figure out the correct side of where a submenu
+		 * will appear and set the attribute to the host accordingly
+		 */
+		const overlayRef = (this._host as any)._parentTrigger.overlayRef;
+		const ps = overlayRef._positionStrategy;
+		const isRoot = this._host.menuStack.peek() === undefined;
+		const side = isRoot
+			? (ps._lastPosition ?? ps._preferredPositions[0]).originY
+			: (ps._lastPosition ?? ps._preferredPositions[0]).originX === 'end'
+			  ? 'right'
+			  : 'left';
+		this._side.set(side);
+	}
+}
