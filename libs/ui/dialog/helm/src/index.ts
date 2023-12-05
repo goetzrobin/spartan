cf20fb3b22ee410d32b14f8ac36fe17c90a37c63
robingotz <tug29225@temple.edu>
2023-12-05 13:39:16 -0500

    feat: integrate brn & hlm for dialog

--- libs/ui/dialog/helm/src/index.ts
+++ libs/ui/dialog/helm/src/index.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 
 import { HlmDialogCloseDirective } from './lib/hlm-dialog-close.directive';
-import { HlmDialogContentDirective } from './lib/hlm-dialog-content.directive';
+import { HlmDialogContentComponent } from './lib/hlm-dialog-content.component';
 import { HlmDialogDescriptionDirective } from './lib/hlm-dialog-description.directive';
 import { HlmDialogFooterComponent } from './lib/hlm-dialog-footer.component';
 import { HlmDialogHeaderComponent } from './lib/hlm-dialog-header.component';
 import { HlmDialogOverlayDirective } from './lib/hlm-dialog-overlay.directive';
 import { HlmDialogTitleDirective } from './lib/hlm-dialog-title.directive';
+import { HlmDialogComponent } from './lib/hlm-dialog.component';
 
 export * from './lib/hlm-dialog-close.directive';
-export * from './lib/hlm-dialog-content.directive';
+export * from './lib/hlm-dialog-content.component';
 export * from './lib/hlm-dialog-description.directive';
 export * from './lib/hlm-dialog-footer.component';
 export * from './lib/hlm-dialog-header.component';
 export * from './lib/hlm-dialog-overlay.directive';
 export * from './lib/hlm-dialog-title.directive';
+export * from './lib/hlm-dialog.component';
 
 export const HlmDialogImports = [
+	HlmDialogComponent,
 	HlmDialogCloseDirective,
-	HlmDialogContentDirective,
+	HlmDialogContentComponent,
 	HlmDialogDescriptionDirective,
 	HlmDialogFooterComponent,
 	HlmDialogHeaderComponent,
---
+++ libs/ui/dialog/helm/src/lib/hlm-dialog-content.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, computed, Input, signal, ViewEncapsulation } from '@angular/core';
+import { radixCross1 } from '@ng-icons/radix-icons';
+import { hlm, injectExposesStateProvider } from '@spartan-ng/ui-core';
+import { BrnDialogCloseDirective } from '@spartan-ng/ui-dialog-brain';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+import { ClassValue } from 'clsx';
+import { HlmDialogCloseDirective } from './hlm-dialog-close.directive';
+
+@Component({
+	selector: 'hlm-dialog-content',
+	standalone: true,
+	imports: [BrnDialogCloseDirective, HlmDialogCloseDirective, HlmIconComponent],
+	providers: [provideIcons({ radixCross1 })],
+	host: {
+		'[class]': '_computedClass()',
+		'[attr.data-state]': 'state()',
+	},
+	template: `
+		<ng-content />
+		<button brnDialogClose hlm>
+			<span class="sr-only">Close</span>
+			<hlm-icon class="flex h-4 w-4" size="100%" name="radixCross1" />
+		</button>
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+})
+export class HlmDialogContentComponent {
+	private readonly _statusProvider = injectExposesStateProvider({ host: true });
+	public readonly state = this._statusProvider.state ?? signal('closed').asReadonly();
+
+	private readonly _userCls = signal<ClassValue>('');
+	protected readonly _computedClass = computed(() =>
+		hlm(
+			'border-border grid w-full max-w-lg relative gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%]  data-[state=open]:slide-in-from-top-[2%] sm:rounded-lg md:w-full',
+			this._userCls(),
+		),
+	);
+
+	@Input()
+	set class(userCls: ClassValue) {
+		this._userCls.set(userCls);
+	}
+}
