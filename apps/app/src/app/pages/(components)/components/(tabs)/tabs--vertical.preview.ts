4b3d19f8d6407efd24f1c6d80d6fb7bd9b366769
elite-benni <benjamin.heiss@elite.tirol>
2023-12-15 08:49:25 +0100

    docs: correct example is code shown for tabs vertical tabs

--- apps/app/src/app/pages/(components)/components/(tabs)/tabs--vertical.preview.ts
+++ apps/app/src/app/pages/(components)/components/(tabs)/tabs--vertical.preview.ts
@@ -41,7 +41,7 @@ import { HlmTabsContentDirective, HlmTabsListDirective, HlmTabsTriggerDirective 
 	},
 	template: `
 		<div brnTabs="account" class="mx-auto flex max-w-3xl flex-row space-x-2" orientation="vertical">
-			<div hlmTabsList hlm orientation="vertical" aria-label="tabs example">
+			<div hlmTabsList orientation="vertical" aria-label="tabs example">
 				<button class="w-full" hlmTabsTrigger="account">Account</button>
 				<button class="w-full" hlmTabsTrigger="password">Password</button>
 				<button class="w-full" hlmTabsTrigger="danger">Danger Zone</button>
@@ -104,5 +104,109 @@ import { HlmTabsContentDirective, HlmTabsListDirective, HlmTabsTriggerDirective 
 })
 export class TabsVerticalPreviewComponent {}
 
-export const verticalCode = `
+export const verticalCode = `import { Component } from '@angular/core';
+import { HlmBadgeDirective } from '@spartan-ng/ui-badge-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import {
+	HlmCardContentDirective,
+	HlmCardDescriptionDirective,
+	HlmCardDirective,
+	HlmCardFooterDirective,
+	HlmCardHeaderDirective,
+	HlmCardTitleDirective,
+} from '@spartan-ng/ui-card-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { BrnTabsDirective } from '@spartan-ng/ui-tabs-brain';
+import { HlmTabsContentDirective, HlmTabsListDirective, HlmTabsTriggerDirective } from '@spartan-ng/ui-tabs-helm';
+
+@Component({
+	selector: 'spartan-tabs-vertical',
+	standalone: true,
+	imports: [
+		BrnTabsDirective,
+
+		HlmTabsListDirective,
+		HlmTabsTriggerDirective,
+		HlmTabsContentDirective,
+
+		HlmCardContentDirective,
+		HlmCardDescriptionDirective,
+		HlmCardDirective,
+		HlmCardFooterDirective,
+		HlmCardHeaderDirective,
+		HlmCardTitleDirective,
+
+		HlmLabelDirective,
+		HlmInputDirective,
+		HlmButtonDirective,
+		HlmBadgeDirective,
+	],
+	host: {
+		class: 'block w-full max-w-lg min-h-[400px]',
+	},
+	template: \`
+		<div brnTabs="account" class="mx-auto flex max-w-3xl flex-row space-x-2" orientation="vertical">
+			<div hlmTabsList orientation="vertical" aria-label="tabs example">
+				<button class="w-full" hlmTabsTrigger="account">Account</button>
+				<button class="w-full" hlmTabsTrigger="password">Password</button>
+				<button class="w-full" hlmTabsTrigger="danger">Danger Zone</button>
+			</div>
+			<div hlmTabsContent="account">
+				<section hlmCard>
+					<div hlmCardHeader>
+						<h3 hlmCardTitle>Account</h3>
+						<p hlmCardDescription>Make changes to your account here. Click save when you're done.</p>
+					</div>
+					<p hlmCardContent>
+						<label class="my-4 block" hlmLabel>
+							Name
+							<input class="mt-1.5 w-full" value="Pedro Duarte" hlmInput />
+						</label>
+						<label class="my-4 block" hlmLabel>
+							Username
+							<input class="mt-1.5 w-full" placeholder="@peduarte" hlmInput />
+						</label>
+					</p>
+					<div hlmCardFooter>
+						<button hlmBtn>Save Changes</button>
+					</div>
+				</section>
+			</div>
+			<div hlmTabsContent="password">
+				<section hlmCard>
+					<div hlmCardHeader>
+						<h3 hlmCardTitle>Password</h3>
+						<p hlmCardDescription>Change your password here. After saving, you'll be logged out.</p>
+					</div>
+					<p hlmCardContent>
+						<label class="my-4 block" hlmLabel>
+							Old Password
+							<input class="mt-1.5 w-full" type="password" hlmInput />
+						</label>
+						<label class="my-4 block" hlmLabel>
+							New Password
+							<input class="mt-1.5 w-full" type="password" hlmInput />
+						</label>
+					</p>
+					<div hlmCardFooter>
+						<button hlmBtn>Save Password</button>
+					</div>
+				</section>
+			</div>
+			<div hlmTabsContent="danger">
+				<section hlmCard>
+					<div hlmCardHeader>
+						<h3 hlmCardTitle>Delete Account</h3>
+						<p hlmCardDescription>Are you sure you want to delete your account? You cannot undo this action.</p>
+					</div>
+					<div hlmCardFooter>
+						<button variant="destructive" hlmBtn>Delete Account</button>
+					</div>
+				</section>
+			</div>
+		</div>
+	\`,
+})
+export class TabsVerticalPreviewComponent {}
 `;
