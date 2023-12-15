bee316d38745aa4863394ec234856b62b5a096d5
Benjamin Heiss <114084395+elite-benni@users.noreply.github.com>
2023-12-15 10:53:35 +0100

    fix(sheet): the HlmSheetModule exports HlmSheetHeadercomponent (#78)

--- libs/ui/sheet/helm/src/index.ts
+++ libs/ui/sheet/helm/src/index.ts
@@ -4,6 +4,7 @@ import { HlmSheetCloseDirective } from './lib/hlm-sheet-close.directive';
 import { HlmSheetContentDirective } from './lib/hlm-sheet-content.directive';
 import { HlmSheetDescriptionDirective } from './lib/hlm-sheet-description.directive';
 import { HlmSheetFooterComponent } from './lib/hlm-sheet-footer.component';
+import { HlmSheetHeaderComponent } from './lib/hlm-sheet-header.component';
 import { HlmSheetOverlayDirective } from './lib/hlm-sheet-overlay.directive';
 import { HlmSheetTitleDirective } from './lib/hlm-sheet-title.directive';
 
@@ -20,6 +21,7 @@ export const HlmSheetImports = [
 	HlmSheetContentDirective,
 	HlmSheetDescriptionDirective,
 	HlmSheetFooterComponent,
+	HlmSheetHeaderComponent,
 	HlmSheetOverlayDirective,
 	HlmSheetTitleDirective,
 ] as const;
