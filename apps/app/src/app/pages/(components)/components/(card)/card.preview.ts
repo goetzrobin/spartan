4fb2f2f832d2416ee7bddf9861736dbf55a8f5cf
dongphuong0905 <phuongtransmile99@gmail.com>
2023-12-21 23:00:00 +0700

    fix(card): update card preview select

--- apps/app/src/app/pages/(components)/components/(card)/card.preview.ts
+++ apps/app/src/app/pages/(components)/components/(card)/card.preview.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { radixCheck, radixChevronDown } from '@ng-icons/radix-icons';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import {
 	HlmCardContentDirective,
@@ -8,13 +9,31 @@ import {
 	HlmCardHeaderDirective,
 	HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
+import { BrnCommandImports } from '@spartan-ng/ui-command-brain';
+import { HlmCommandImports } from '@spartan-ng/ui-command-helm';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import {
+	BrnPopoverComponent,
+	BrnPopoverContentDirective,
+	BrnPopoverTriggerDirective,
+} from '@spartan-ng/ui-popover-brain';
+import { HlmPopoverContentDirective } from '@spartan-ng/ui-popover-helm';
+
+type Framework = { label: string; value: string };
 
 @Component({
 	selector: 'spartan-card-preview',
 	standalone: true,
 	imports: [
+		BrnCommandImports,
+		HlmCommandImports,
+		HlmIconComponent,
+		BrnPopoverComponent,
+		BrnPopoverTriggerDirective,
+		BrnPopoverContentDirective,
+		HlmPopoverContentDirective,
 		HlmCardDirective,
 		HlmCardHeaderDirective,
 		HlmCardTitleDirective,
@@ -25,6 +44,7 @@ import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 		HlmCardFooterDirective,
 		HlmButtonDirective,
 	],
+	providers: [provideIcons({ radixCheck, radixChevronDown })],
 	template: `
 		<section class="w-80" hlmCard>
 			<div hlmCardHeader>
@@ -37,13 +57,39 @@ import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 					<input class="mt-1.5 w-full" placeholder="Name of your project" hlmInput />
 				</label>
 
-				<label class="my-4 block" hlmLabel>
+				<label class="my-4 mb-1.5 block" hlmLabel>
 					Framework
-					<select class="mt-1.5 w-full" hlmInput>
-						<option>Angular</option>
-						<option>React</option>
-						<option>Vue</option>
-					</select>
+
+					<brn-popover [state]="state()" (stateChanged)="stateChanged($event)" sideOffset="5" closeDelay="100">
+						<button
+							class="mt-1.5 w-full justify-between"
+							id="edit-profile"
+							variant="outline"
+							brnPopoverTrigger
+							(click)="state.set('open')"
+							hlmBtn
+						>
+							{{ currentFramework() ? currentFramework()?.label : 'Select' }}
+							<hlm-icon size="sm" name="radixChevronDown" />
+						</button>
+						<brn-cmd *brnPopoverContent="let ctx" hlmPopoverContent hlm class="w-[270px] p-0">
+							<div *brnCmdEmpty hlmCmdEmpty>No results found.</div>
+							<brn-cmd-list hlm>
+								<brn-cmd-group hlm>
+									@for (framework of frameworks; track framework) {
+										<button brnCmdItem [value]="framework.value" (selected)="commandSelected(framework)" hlm>
+											<hlm-icon
+												[class.opacity-0]="currentFramework()?.value !== framework.value"
+												name="radixCheck"
+												hlmCmdIcon
+											/>
+											{{ framework.label }}
+										</button>
+									}
+								</brn-cmd-group>
+							</brn-cmd-list>
+						</brn-cmd>
+					</brn-popover>
 				</label>
 			</p>
 			<div hlmCardFooter class="justify-between">
@@ -53,10 +99,55 @@ import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 		</section>
 	`,
 })
-export class CardPreviewComponent {}
+export class CardPreviewComponent {
+	public frameworks = [
+		{
+			label: 'AnalogJs',
+			value: 'analogjs',
+		},
+		{
+			label: 'Angular',
+			value: 'angular',
+		},
+		{
+			label: 'Vue',
+			value: 'vue',
+		},
+		{
+			label: 'Nuxt',
+			value: 'nuxt',
+		},
+		{
+			label: 'React',
+			value: 'react',
+		},
+		{
+			label: 'NextJs',
+			value: 'nextjs',
+		},
+	];
+
+	public currentFramework = signal<Framework | undefined>(undefined);
+	public state = signal<'closed' | 'open'>('closed');
+
+	stateChanged(state: 'open' | 'closed') {
+		this.state.set(state);
+	}
+
+	commandSelected(framework: Framework) {
+		this.state.set('closed');
+		if (this.currentFramework()?.value === framework.value) {
+			this.currentFramework.set(undefined);
+		} else {
+			this.currentFramework.set(framework);
+		}
+	}
+}
 
 export const defaultCode = `
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { radixCheck, radixChevronDown } from '@ng-icons/radix-icons';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import {
   HlmCardContentDirective,
   HlmCardDescriptionDirective,
@@ -65,14 +156,31 @@ import {
   HlmCardHeaderDirective,
   HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
-import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { BrnCommandImports } from '@spartan-ng/ui-command-brain';
+import { HlmCommandImports } from '@spartan-ng/ui-command-helm';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmPopoverContentDirective } from '@spartan-ng/ui-popover-helm';
+import {
+  BrnPopoverComponent,
+  BrnPopoverContentDirective,
+  BrnPopoverTriggerDirective,
+} from '@spartan-ng/ui-popover-brain';
+
+type Framework = { label: string; value: string };
 
 @Component({
   selector: 'spartan-card-preview',
   standalone: true,
   imports: [
+    BrnCommandImports,
+    HlmCommandImports,
+    HlmIconComponent,
+    BrnPopoverComponent,
+    BrnPopoverTriggerDirective,
+    BrnPopoverContentDirective,
+    HlmPopoverContentDirective,
     HlmCardDirective,
     HlmCardHeaderDirective,
     HlmCardTitleDirective,
@@ -83,35 +191,106 @@ import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
     HlmCardFooterDirective,
     HlmButtonDirective,
   ],
+  providers: [provideIcons({ radixCheck, radixChevronDown })],
   template: \`
-    <section class="w-80" hlmCard>
-      <div hlmCardHeader>
-        <h3 hlmCardTitle>Create new project</h3>
-        <p hlmCardDescription>Deploy your new project in one-click.</p>
-      </div>
-      <p hlmCardContent>
-        <label class="block" hlmLabel
-          >Name
-          <input class="w-full mt-1.5" placeholder="Name of your project" hlmInput />
-        </label>
+      <section class="w-80" hlmCard>
+          <div hlmCardHeader>
+              <h3 hlmCardTitle>Create new project</h3>
+              <p hlmCardDescription>Deploy your new project in one-click.</p>
+          </div>
+          <p hlmCardContent>
+              <label class="block" hlmLabel>
+                  Name
+                  <input class="mt-1.5 w-full" placeholder="Name of your project" hlmInput />
+              </label>
 
-        <label class="block my-4" hlmLabel
-          >Framework
-          <select class="w-full mt-1.5" hlmInput>
-            <option>Angular</option>
-            <option>React</option>
-            <option>Vue</option>
-          </select>
-        </label>
-      </p>
-      <div hlmCardFooter class="justify-between">
-        <button hlmBtn variant="ghost">Cancel</button>
-        <button hlmBtn>Create</button>
-      </div>
-    </section>
+              <label class="my-4 mb-1.5 block" hlmLabel>
+                  Framework
+
+                  <brn-popover [state]="state()" (stateChanged)="stateChanged($event)" sideOffset="5" closeDelay="100">
+                      <button
+                              class="w-full mt-1.5 justify-between"
+                              id="edit-profile"
+                              variant="outline"
+                              brnPopoverTrigger
+                              (click)="state.set('open')"
+                              hlmBtn
+                      >
+                          {{ currentFramework() ? currentFramework()?.label : 'Select' }}
+                          <hlm-icon size="sm" name="radixChevronDown" />
+                      </button>
+                      <brn-cmd *brnPopoverContent="let ctx" hlmPopoverContent hlm class="w-[270px] p-0">
+                          <div *brnCmdEmpty hlmCmdEmpty>No results found.</div>
+                          <brn-cmd-list hlm>
+                              <brn-cmd-group hlm>
+                                  @for (framework of frameworks;track framework) {
+                                      <button brnCmdItem [value]="framework.value"
+                                              (selected)="commandSelected(framework)" hlm>
+                                          <hlm-icon
+                                                  [class.opacity-0]="currentFramework()?.value !== framework.value"
+                                                  name="radixCheck"
+                                                  hlmCmdIcon
+                                          />
+                                          {{ framework.label }}
+                                      </button>
+                                  }
+                              </brn-cmd-group>
+                          </brn-cmd-list>
+                      </brn-cmd>
+                  </brn-popover>
+              </label>
+          </p>
+          <div hlmCardFooter class="justify-between">
+              <button hlmBtn variant="ghost">Cancel</button>
+              <button hlmBtn>Create</button>
+          </div>
+      </section>
   \`,
 })
-export class CardPreviewComponent {}
+export class CardPreviewComponent {
+  public frameworks = [
+    {
+      label: 'AnalogJs',
+      value: 'analogjs'
+    },
+    {
+      label: 'Angular',
+      value: 'angular'
+    },
+    {
+      label: 'Vue',
+      value: 'vue'
+    },
+    {
+      label: 'Nuxt',
+      value: 'nuxt'
+    },
+    {
+      label: 'React',
+      value: 'react'
+    },
+    {
+      label: 'NextJs',
+      value: 'nextjs'
+    }
+  ];
+
+  public currentFramework = signal<Framework | undefined>(undefined);
+  public state = signal<'closed' | 'open'>('closed');
+
+  stateChanged(state: 'open' | 'closed') {
+    this.state.set(state);
+  }
+
+  commandSelected(framework: Framework) {
+    this.state.set('closed');
+    if (this.currentFramework()?.value === framework.value) {
+      this.currentFramework.set(undefined);
+    } else {
+      this.currentFramework.set(framework);
+    }
+  }
+}
 `;
 
 export const defaultImports = `
