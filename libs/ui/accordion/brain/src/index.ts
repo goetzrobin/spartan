eb46c518cf7fb109c9b813321a71c32b84404162
Sammy Mohamed <sammymohamed1223@gmail.com>
2023-12-23 22:13:09 -0500

    refactor(accordion): brn-accordion-trigger

--- libs/ui/accordion/brain/src/index.ts
+++ libs/ui/accordion/brain/src/index.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { BrnAccordionContentComponent } from './lib/brn-accordion-content.component';
 import { BrnAccordionItemDirective } from './lib/brn-accordion-item.directive';
-import { BrnAccordionTriggerDirective } from './lib/brn-accordion-trigger.directive';
+import { BrnAccordionTriggerComponent } from './lib/brn-accordion-trigger.component';
 import { BrnAccordionDirective } from './lib/brn-accordion.directive';
 
 export * from './lib/brn-accordion-content.component';
 export * from './lib/brn-accordion-item.directive';
-export * from './lib/brn-accordion-trigger.directive';
+export * from './lib/brn-accordion-trigger.component';
 export * from './lib/brn-accordion.directive';
 
 export const BrnAccordionImports = [
 	BrnAccordionDirective,
 	BrnAccordionContentComponent,
 	BrnAccordionItemDirective,
-	BrnAccordionTriggerDirective,
+	BrnAccordionTriggerComponent,
 ] as const;
 
 @NgModule({
--- libs/ui/accordion/brain/src/lib/brn-accordion-trigger.component.ts
+++ libs/ui/accordion/brain/src/lib/brn-accordion-trigger.component.ts
@@ -1,31 +1,56 @@
-import { Directive, ElementRef, inject } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	DestroyRef,
+	ElementRef,
+	HostBinding,
+	Input,
+	OnInit,
+	ViewChild,
+	inject,
+	signal,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { rxHostPressedListener } from '@spartan-ng/ui-core';
 import { fromEvent } from 'rxjs';
 import { BrnAccordionItemDirective } from './brn-accordion-item.directive';
 import { BrnAccordionDirective } from './brn-accordion.directive';
 
-@Directive({
-	selector: '[brnAccordionTrigger]',
+@Component({
+	selector: 'brn-accordion-trigger, hlm-accordion-trigger',
 	standalone: true,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: {
-		'[attr.data-state]': 'state()',
-		'[attr.aria-expanded]': 'state() === "open"',
-		'[attr.aria-controls]': 'ariaControls',
+		'[style]': '"display:contents"',
 		role: 'heading',
 		'aria-level': '3',
-		'[id]': 'id',
 	},
+	template: `
+		<button
+			#btn
+			[class]="_contentClass()"
+			[attr.data-state]="state()"
+			[attr.aria-expanded]="state() === 'open'"
+			[attr.aria-controls]="ariaControls"
+			[id]="id"
+		>
+			<ng-content />
+		</button>
+	`,
 })
-export class BrnAccordionTriggerDirective {
+export class BrnAccordionTriggerComponent implements OnInit {
 	private readonly _accordion = inject(BrnAccordionDirective);
 	private readonly _item = inject(BrnAccordionItemDirective);
-	private readonly _elementRef = inject(ElementRef);
 	private readonly _hostPressedListener = rxHostPressedListener();
+	private readonly _destroyRef = inject(DestroyRef);
 
 	public readonly state = this._item.state;
 	public readonly id = 'brn-accordion-trigger-' + this._item.id;
 	public readonly ariaControls = 'brn-accordion-content-' + this._item.id;
+
+	protected readonly _contentClass = signal('');
+
+	@ViewChild('btn', { static: true }) trigger!: ElementRef<HTMLButtonElement>;
 
 	constructor() {
 		if (!this._accordion) {
@@ -38,15 +63,22 @@ export class BrnAccordionTriggerDirective {
 		this._hostPressedListener.subscribe(() => {
 			this._accordion.toggleItem(this._item.id);
 		});
+	}
 
-		fromEvent(this._elementRef.nativeElement, 'focus')
-			.pipe(takeUntilDestroyed())
-			.subscribe(() => {
-				this._accordion.setActiveItem(this);
-			});
+	ngOnInit(): void {
+		fromEvent(this.trigger.nativeElement, 'focus')
+			.pipe(takeUntilDestroyed(this._destroyRef))
+			.subscribe(() => this._accordion.setActiveItem(this));
 	}
 
 	public focus() {
-		this._elementRef.nativeElement.focus();
+		this.trigger.nativeElement.focus();
+	}
+
+	// eslint-disable-next-line @angular-eslint/no-input-rename
+	@HostBinding('attr.class')
+	@Input()
+	set class(inputs: string) {
+		this._contentClass.set(inputs);
 	}
 }
