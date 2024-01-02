3bf859cafaa385cdb94fe2a917429cf7bb720ca0
Brandon Roberts <robertsbt@gmail.com>
2024-01-02 10:09:54 -0600

    feat(trpc): add trpc lib and e2e test suite (#94)

--- commitlint.config.js
+++ commitlint.config.js
@@ -47,6 +47,7 @@ module.exports = {
 				'toast',
 				'toggle',
 				'tooltip',
+				'trpc',
 				'typography',
 				'nx',
 			],
---
+++ libs/trpc/vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	return {
+		root: 'src',
+		test: {
+			passWithNoTests: true,
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: ['src/test-setup.ts'],
+			include: ['**/*.spec.ts'],
+			cache: {
+				dir: `../../node_modules/.vitest`,
+			},
+		},
+		define: {
+			'import.meta.vitest': mode !== 'production',
+		},
+	};
+});
