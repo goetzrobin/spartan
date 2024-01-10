1da0b61fbb45668a334d8cbeefc70ef4feaaf80f
Dominik Hladík <dominik.hladik@seznam.cz>
2024-01-10 23:36:03 +0100

    chore(spinner): missing spinner stories added

---
+++ libs/ui/spinner/spinner.stories.ts
@@ -0,0 +1,32 @@
+import { Meta, StoryObj, argsToTemplate, moduleMetadata } from '@storybook/angular';
+import { HlmSpinnerComponent } from './helm/src';
+
+const meta: Meta<HlmSpinnerComponent> = {
+	title: 'Spinner',
+	component: HlmSpinnerComponent,
+	tags: ['autodocs'],
+	argTypes: {
+		size: {
+			options: ['default', 'xs', 'sm'],
+			control: {
+				type: 'select',
+			},
+		},
+	},
+	decorators: [
+		moduleMetadata({
+			imports: [HlmSpinnerComponent],
+		}),
+	],
+	render: ({ ...args }) => ({
+		props: args,
+		template: `
+    <hlm-spinner ${argsToTemplate(args)}></hlm-spinner>
+    `,
+	}),
+};
+
+export default meta;
+type Story = StoryObj<HlmSpinnerComponent>;
+
+export const Default: Story = {};
