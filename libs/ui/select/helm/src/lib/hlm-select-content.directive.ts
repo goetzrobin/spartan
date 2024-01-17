414856ae3313eddf296221bb2e2f4c4daf92188e
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-17 00:52:32 -0500

    fix(select): tw scroll class

--- libs/ui/select/helm/src/lib/hlm-select-content.directive.ts
+++ libs/ui/select/helm/src/lib/hlm-select-content.directive.ts
@@ -12,7 +12,7 @@ import { ClassValue } from 'clsx';
 })
 export class HlmSelectContentDirective {
 	baseClasses =
-		'top-[2px] relative z-50 min-w-[8rem] overflow-scroll rounded-md border bg-popover text-popover-foreground shadow-md p-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2';
+		'top-[2px] relative z-50 min-w-[8rem] overflow-auto rounded-md border bg-popover text-popover-foreground shadow-md p-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2';
 
 	private readonly classNames = signal<ClassValue>('');
 
