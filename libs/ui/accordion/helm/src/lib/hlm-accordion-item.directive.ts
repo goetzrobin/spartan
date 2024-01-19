87eb8e5d4c30439f227c4fb4da1bbf6a9c4641e0
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-19 13:58:51 -0500

    fix(accordion): re-adds brn+hlm selector to accordion-item (#130)

--- libs/ui/accordion/helm/src/lib/hlm-accordion-item.directive.ts
+++ libs/ui/accordion/helm/src/lib/hlm-accordion-item.directive.ts
@@ -4,7 +4,7 @@ import { hlm } from '@spartan-ng/ui-core';
 import { ClassValue } from 'clsx';
 
 @Directive({
-	selector: '[hlmAccordionItem],hlm-accordion-item',
+	selector: '[hlmAccordionItem],brn-accordion-item[hlm],hlm-accordion-item',
 	standalone: true,
 	host: {
 		'[class]': '_computedClass()',
