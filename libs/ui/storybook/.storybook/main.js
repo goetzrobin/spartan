a315e616d352cb12bbe161403a8dbc9ff872b80e
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-21 17:14:33 -0500

    fix: remove storybook addon-styling object notation

--- libs/ui/storybook/.storybook/main.js
+++ libs/ui/storybook/.storybook/main.js
@@ -1,14 +1,6 @@
 const config = {
 	stories: ['../../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	addons: [
-		'@storybook/addon-essentials',
-		'@storybook/addon-a11y',
-		{
-			name: '@storybook/addon-styling',
-			// As recommended by addon, cannot use addon options with angular
-			// Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-		},
-	],
+	addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-styling'],
 	framework: {
 		name: '@storybook/angular',
 		options: {},
