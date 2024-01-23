65fa7b07782f6a2a13270076cc7056a520bb81a4
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-23 00:04:34 -0500

    feat(select): add hlm select-scroll-up/down

--- libs/ui/select/helm/src/index.ts
+++ libs/ui/select/helm/src/index.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { HlmSelectContentDirective } from './lib/hlm-select-content.directive';
 import { HlmSelectOptionComponent } from './lib/hlm-select-option.component';
+import { HlmSelectScrollDownDirective } from './lib/hlm-select-scroll-down.directive';
+import { HlmSelectScrollUpDirective } from './lib/hlm-select-scroll-up.directive';
 import { HlmSelectTriggerComponent } from './lib/hlm-select-trigger.component';
 import { HlmSelectValueDirective } from './lib/hlm-select-value.directive';
 import { HlmSelectDirective } from './lib/hlm-select.directive';
 
 export * from './lib/hlm-select-content.directive';
 export * from './lib/hlm-select-option.component';
+export * from './lib/hlm-select-scroll-down.directive';
+export * from './lib/hlm-select-scroll-up.directive';
 export * from './lib/hlm-select-trigger.component';
 export * from './lib/hlm-select-value.directive';
 export * from './lib/hlm-select.directive';
@@ -17,6 +21,8 @@ export const HlmSelectImports = [
 	HlmSelectOptionComponent,
 	HlmSelectValueDirective,
 	HlmSelectDirective,
+	HlmSelectScrollUpDirective,
+	HlmSelectScrollDownDirective,
 ] as const;
 
 @NgModule({
