dfdba163b16fb862636aeac1ee5fea3f55a00490
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-28 07:30:41 -0500

    fix(select): selected options value display

--- libs/ui/select/brain/src/lib/brn-select-value.component.ts
+++ libs/ui/select/brain/src/lib/brn-select-value.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { BrnSelectService } from './brn-select.service';
 
 @Component({
@@ -8,13 +8,26 @@ import { BrnSelectService } from './brn-select.service';
 			{{ value() && value()?.length ? value() : placeholder() }}
 		</span>
 	`,
+	styles: [
+		`
+			:host {
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+				-webkit-line-clamp: 1;
+			}
+		`,
+	],
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BrnSelectValueComponent {
 	private _selectService = inject(BrnSelectService);
 
-	readonly value = this._selectService.value;
+	readonly _value = this._selectService.selectedOptions;
+	readonly value = computed(() => {
+		const selectedLabels = this._value().map((selectedOption) => selectedOption.option?.getLabel());
+		return selectedLabels.join(', ');
+	});
 
 	readonly placeholder = this._selectService.placeholder;
 }
