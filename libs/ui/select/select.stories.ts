8a263f693cd85415c183fb21455e82a805f952da
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-28 15:38:29 -0500

    feat(select): sticky labels

--- libs/ui/select/select.stories.ts
+++ libs/ui/select/select.stories.ts
@@ -232,3 +232,83 @@ export const Scrollable: Story = {
   <form>`,
 	}),
 };
+
+export const ScrollableWithStickyLabels: Story = {
+	render: (args) => ({
+		props: { ...args, myform: new FormGroup({ timezone: new FormControl() }) },
+		template: `
+    <form [formGroup]="myform">
+	<hlm-select formControlName="timezone" ${argsToTemplate(args)}>
+		<hlm-select-trigger class="w-[280px]">
+			<hlm-select-value />
+		</hlm-select-trigger>
+    	<hlm-select-content class="min-w-content max-h-96" [stickyLabels]="true">
+		<hlm-select-scroll-up>
+			<hlm-icon class="ml-2 h-4 w-4" name="radixChevronUp" /></hlm-select-scroll-up>
+
+			<hlm-select-group>
+				<hlm-select-label>North America</hlm-select-label>
+				<hlm-option value="est">Eastern Standard Time (EST)</hlm-option>
+				<hlm-option value="cst">Central Standard Time (CST)</hlm-option>
+				<hlm-option value="mst">Mountain Standard Time (MST)</hlm-option>
+				<hlm-option value="pst">Pacific Standard Time (PST)</hlm-option>
+				<hlm-option value="akst">Alaska Standard Time (AKST)</hlm-option>
+				<hlm-option value="hst">Hawaii Standard Time (HST)</hlm-option>
+			</hlm-select-group>
+
+			<hlm-select-group>
+				<hlm-select-label>Europe & Africa</hlm-select-label>
+				<hlm-option value="gmt">Greenwich Mean Time (GMT)</hlm-option>
+				<hlm-option value="cet">Central European Time (CET)</hlm-option>
+				<hlm-option value="eet">Eastern European Time (EET)</hlm-option>
+				<hlm-option value="west">
+				  Western European Summer Time (WEST)
+				</hlm-option>
+				<hlm-option value="cat">Central Africa Time (CAT)</hlm-option>
+				<hlm-option value="eat">East Africa Time (EAT)</hlm-option>
+
+
+			</hlm-select-group>
+
+			<hlm-select-group>
+				<hlm-select-label>Asia</hlm-select-label>
+				<hlm-option value="msk">Moscow Time (MSK)</hlm-option>
+				<hlm-option value="ist">India Standard Time (IST)</hlm-option>
+				<hlm-option value="cst_china">China Standard Time (CST)</hlm-option>
+				<hlm-option value="jst">Japan Standard Time (JST)</hlm-option>
+				<hlm-option value="kst">Korea Standard Time (KST)</hlm-option>
+				<hlm-option value="ist_indonesia">
+				  Indonesia Central Standard Time (WITA)
+				</hlm-option>
+			</hlm-select-group>
+
+			<hlm-select-group>
+				<hlm-select-label>Australia & Pacific</hlm-select-label>
+				<hlm-option value="awst">
+				Australian Western Standard Time (AWST)
+			  </hlm-option>
+			  <hlm-option value="acst">
+				Australian Central Standard Time (ACST)
+			  </hlm-option>
+			  <hlm-option value="aest">
+				Australian Eastern Standard Time (AEST)
+			  </hlm-option>
+			  <hlm-option value="nzst">New Zealand Standard Time (NZST)</hlm-option>
+			  <hlm-option value="fjt">Fiji Time (FJT)</hlm-option>
+	
+			</hlm-select-group>
+
+			<hlm-select-group>
+				<hlm-select-label>South America</hlm-select-label>
+				<hlm-option value="art">Argentina Time (ART)</hlm-option>
+				<hlm-option value="bot">Bolivia Time (BOT)</hlm-option>
+				<hlm-option value="brt">Brasilia Time (BRT)</hlm-option>
+				<hlm-option value="clt">Chile Standard Time (CLT)</hlm-option>
+			</hlm-select-group>
+
+			<hlm-select-scroll-down><hlm-icon class="ml-2 h-4 w-4" name="radixChevronDown" /></hlm-select-scroll-down>
+		</hlm-select-content>
+	</hlm-select>
+  <form>`,
+	}),
+};
