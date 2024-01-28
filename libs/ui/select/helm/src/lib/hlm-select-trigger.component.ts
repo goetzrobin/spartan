44c27c486f7cd0e64f4d9b179e6c78db6f35c3fb
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-28 00:39:06 -0500

    feat(select): select-trigger custom icon

--- libs/ui/select/helm/src/lib/hlm-select-trigger.component.ts
+++ libs/ui/select/helm/src/lib/hlm-select-trigger.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, Input, ViewChild, computed, signal } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, ViewChild, computed, signal } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
-import { radixChevronDown } from '@ng-icons/radix-icons';
+import { radixCaretSort } from '@ng-icons/radix-icons';
 import { hlm } from '@spartan-ng/ui-core';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { BrnSelectTriggerDirective } from '@spartan-ng/ui-select-brain';
@@ -10,16 +10,23 @@ import { ClassValue } from 'clsx';
 	selector: 'hlm-select-trigger',
 	standalone: true,
 	imports: [BrnSelectTriggerDirective, HlmIconComponent],
-	providers: [provideIcons({ radixChevronDown })],
+	providers: [provideIcons({ radixCaretSort })],
 	template: `
 		<button [class]="_computedClass()" #button brnSelectTrigger type="button">
 			<ng-content />
-			<hlm-icon class="ml-2 h-4 w-4" name="radixChevronDown" />
+			@if (icon) {
+				<ng-content select="hlm-icon" />
+			} @else {
+				<hlm-icon class="ml-2 h-4 w-4" name="radixCaretSort" />
+			}
 		</button>
 	`,
 })
 export class HlmSelectTriggerComponent {
 	@ViewChild('button') buttonEl!: ElementRef;
+
+	@ContentChild(HlmIconComponent, { static: false })
+	protected icon!: HlmIconComponent;
 
 	baseClasses =
 		'flex h-10 items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-[180px]';
