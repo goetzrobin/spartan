aaf12d1c2944f476a67d8ecc7dab56b8dcb57495
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-01-31 17:49:32 +0100

    feat(pagination): update storybook

--- libs/ui/pagination/pagination.stories.ts
+++ libs/ui/pagination/pagination.stories.ts
@@ -1,3 +1,4 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import type { Meta, StoryObj } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
 import { HlmPaginationDirective, HlmPaginationImports } from './helm/src';
@@ -8,7 +9,7 @@ const meta: Meta<HlmPaginationDirective> = {
 	tags: ['autodocs'],
 	decorators: [
 		moduleMetadata({
-			imports: [HlmPaginationImports],
+			imports: [HlmPaginationImports, RouterTestingModule],
 		}),
 	],
 };
@@ -19,58 +20,28 @@ type Story = StoryObj<HlmPaginationDirective>;
 export const Default: Story = {
 	render: () => ({
 		template: `
-    <section class='max-w-lg mx-auto' hlmCard>
-       <div hlmCardHeader>
-        <h3 hlmCardTitle>Create new project</h3>
-        <p hlmCardDescription>
-          Deploy your new project in one-click.
-        </p>
-      </div>
-      <p hlmCardContent>
-       <label class='block' hlmLabel>Name
-       <input class='w-full mt-1.5' placeholder='Name of your project' hlmInput>
-       </label>
-
-       <label class='block my-4' hlmLabel>Framework
-       <select class='w-full mt-1.5' hlmInput>
-        <option>Angular</option>
-        <option>React</option>
-        <option>Vue</option>
-       </select>
-       </label>
-      </p>
-      <div hlmCardFooter class='justify-between'>
-        <button hlmBtn variant='ghost'>Cancel</button>
-        <button hlmBtn>Create</button>
-      </div>
-    </section>
-    `,
-	}),
-};
-
-export const Transposed: Story = {
-	render: () => ({
-		template: `
-    <section class='max-w-lg mx-auto' hlmCard>
-       <div hlmCardHeader direction='row'>
-        <h3 hlmCardTitle>AngularGPT</h3>
-        <p hlmCardDescription>
-          <span variant='secondary' hlmBadge>beta</span>
-        </p>
-      </div>
-      <p hlmCardContent>
-       <label class='block' hlmLabel>E-mail
-       <input class='w-full mt-1.5' placeholder='you@spartan-ng' hlmInput>
-       </label>
-         <label class='block my-4' hlmLabel>Password
-       <input class='w-full mt-1.5' type='password' hlmInput>
-       </label>
-      </p>
-      <div hlmCardFooter direction='column'>
-        <button hlmBtn>Sign In</button>
-        <button hlmBtn variant='ghost'>Create Account</button>
-      </div>
-    </section>
+    <nav hlmPagination>
+      <ul hlmPaginationContent>
+        <li hlmPaginationItem>
+          <hlm-pagination-previous href="#" />
+        </li>
+        <li hlmPaginationItem>
+          <a routerLink="#" hlmPaginationLink>1</a>
+        </li>
+        <li hlmPaginationItem>
+          <a routerLink="#" hlmPaginationLink isActive>2</a>
+        </li>
+        <li hlmPaginationItem>
+          <a routerLink="#" hlmPaginationLink>3</a>
+        </li>
+        <li hlmPaginationItem>
+          <hlm-pagination-ellipsis />
+        </li>
+        <li hlmPaginationItem>
+          <hlm-pagination-next href="#" />
+        </li>
+      </ul>
+    </nav>
     `,
 	}),
 };
