2595aa2cf92d75c8d776d7b7aa5668267a63d803
Το ΌνÎTheophilos Matzavinos <theophilos.matzavinos@ctengineering.de>
2024-02-17 01:28:55 +0200

    feat(carousel): initial commit

---
+++ libs/ui/carousel/carousel.stories.ts
@@ -0,0 +1,79 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+
+import { HlmCardContentDirective, HlmCardDirective } from '../card/helm/src';
+import { HlmCarouselComponent, HlmCarouselImports } from './helm/src';
+
+const meta: Meta<HlmCarouselComponent> = {
+	title: 'Carousel',
+	component: HlmCarouselComponent,
+	tags: ['autodocs'],
+	decorators: [
+		moduleMetadata({
+			imports: [HlmCarouselImports, HlmCardDirective, HlmCardContentDirective],
+		}),
+	],
+};
+
+export default meta;
+type Story = StoryObj<HlmCarouselComponent>;
+
+export const Default: Story = {
+	render: () => ({
+		template: `
+    <div class="flex justify-center items-center w-full p-4">
+      <hlm-carousel class="w-full max-w-xs">
+        <hlm-carousel-content>
+          <hlm-carousel-item>
+            <div class="p-1">
+              <section hlmCard>
+                <p hlmCardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">1</span>
+                </p>
+              </section>
+            </div>
+          </hlm-carousel-item>
+          <hlm-carousel-item>
+            <div class="p-1">
+              <section hlmCard>
+                <p hlmCardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">2</span>
+                </p>
+              </section>
+            </div>
+          </hlm-carousel-item>
+          <hlm-carousel-item>
+            <div class="p-1">
+              <section hlmCard>
+                <p hlmCardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">3</span>
+                </p>
+              </section>
+            </div>
+          </hlm-carousel-item>
+          <hlm-carousel-item>
+            <div class="p-1">
+              <section hlmCard>
+                <p hlmCardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">4</span>
+                </p>
+              </section>
+            </div>
+          </hlm-carousel-item>
+          <hlm-carousel-item>
+            <div class="p-1">
+              <section hlmCard>
+                <p hlmCardContent class="flex aspect-square items-center justify-center p-6">
+                  <span class="text-4xl font-semibold">5</span>
+                </p>
+              </section>
+            </div>
+          </hlm-carousel-item>
+        </hlm-carousel-content>
+        <button hlm-carousel-previous></button>
+        <button hlm-carousel-next></button>
+      </hlm-carousel>
+    </div>
+    `,
+	}),
+};
