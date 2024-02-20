83310cf3fbb197b8a6cbfefa9452e71cb0a2c7d7
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-02-20 11:50:34 +0100

    refactor(tabs): use input signals
    * pass disabled prop correctly from hlmTabsTrigger to brnTabsTrigger
    * change _userClass to public, vscode shows errors for class in template otherwise

--- libs/ui/tabs/helm/src/lib/hlm-tabs-trigger.directive.ts
+++ libs/ui/tabs/helm/src/lib/hlm-tabs-trigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, computed, inject, input } from '@angular/core';
+import { Directive, computed, input } from '@angular/core';
 import { hlm } from '@spartan-ng/ui-core';
 import { BrnTabsTriggerDirective } from '@spartan-ng/ui-tabs-brain';
 import { ClassValue } from 'clsx';
@@ -6,22 +6,15 @@ import { ClassValue } from 'clsx';
 @Directive({
 	selector: '[hlmTabsTrigger]',
 	standalone: true,
+	hostDirectives: [{ directive: BrnTabsTriggerDirective, inputs: ['brnTabsTrigger: hlmTabsTrigger', 'disabled'] }],
 	host: {
 		'[class]': '_computedClass()',
 	},
-	hostDirectives: [BrnTabsTriggerDirective],
 })
 export class HlmTabsTriggerDirective {
-	private readonly _brn = inject(BrnTabsTriggerDirective);
+	public readonly triggerFor = input.required<string>({ alias: 'hlmTabsTrigger' });
 
-	@Input('hlmTabsTrigger')
-	set triggerFor(key: string) {
-		if (this._brn) {
-			this._brn.triggerFor = key;
-		}
-	}
-
-	private readonly _userClass = input<ClassValue>('', { alias: 'class' });
+	public readonly _userClass = input<ClassValue>('', { alias: 'class' });
 	protected _computedClass = computed(() =>
 		hlm(
 			'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
