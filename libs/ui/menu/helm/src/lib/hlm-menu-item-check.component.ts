4059f3fc63adb8c086e3f1e6ad5f30fa222e171c
robingotz <tug29225@temple.edu>
2024-03-05 14:31:05 +0100

    chore: prettify hlm-menu-item-check and hlm-menu-item-radio

--- libs/ui/menu/helm/src/lib/hlm-menu-item-check.component.ts
+++ libs/ui/menu/helm/src/lib/hlm-menu-item-check.component.ts
@@ -20,6 +20,9 @@ import { ClassValue } from 'clsx';
 export class HlmMenuItemCheckComponent {
 	public readonly userClass = input<ClassValue>('', { alias: 'class' });
 	protected _computedClass = computed(() =>
-		hlm('group-[.checked]:opacity-100 opacity-0 absolute left-2 flex h-3.5 w-3.5 items-center justify-center', this.userClass()),
+		hlm(
+			'group-[.checked]:opacity-100 opacity-0 absolute left-2 flex h-3.5 w-3.5 items-center justify-center',
+			this.userClass(),
+		),
 	);
 }
