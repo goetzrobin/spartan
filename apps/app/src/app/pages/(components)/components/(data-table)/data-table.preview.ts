23bc29e4f2affc0b44aac68fedb57e059a700430
ragul.u <ragul.u@kgisl.com>
2024-03-29 11:47:47 +0530

    Data Table Preview Component - Code formatted

--- apps/app/src/app/pages/(components)/components/(data-table)/data-table.preview.ts
+++ apps/app/src/app/pages/(components)/components/(data-table)/data-table.preview.ts
@@ -10,12 +10,12 @@ import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
 import { HlmMenuModule } from '@spartan-ng/ui-menu-helm';
+import { BrnSelectModule } from '@spartan-ng/ui-select-brain';
+import { HlmSelectModule } from '@spartan-ng/ui-select-helm';
 import { BrnTableModule, PaginatorState, useBrnColumnManager } from '@spartan-ng/ui-table-brain';
 import { HlmTableModule } from '@spartan-ng/ui-table-helm';
 import { hlmMuted } from '@spartan-ng/ui-typography-helm';
 import { debounceTime, map } from 'rxjs';
-import { BrnSelectModule } from '@spartan-ng/ui-select-brain';
-import { HlmSelectModule } from '@spartan-ng/ui-select-helm';
 
 export type Payment = {
 	id: string;
@@ -167,10 +167,10 @@ const PAYMENT_DATA: Payment[] = [
 		HlmInputDirective,
 
 		HlmCheckboxCheckIconComponent,
-    HlmCheckboxComponent,
-    
-    BrnSelectModule,
-    HlmSelectModule
+		HlmCheckboxComponent,
+
+		BrnSelectModule,
+		HlmSelectModule,
 	],
 	providers: [provideIcons({ lucideChevronDown, lucideMoreHorizontal, lucideArrowUpDown })],
 	host: {
@@ -277,20 +277,23 @@ const PAYMENT_DATA: Payment[] = [
 		>
 			<span class="${hlmMuted} text-sm">{{ _selected().length }} of {{ _totalElements() }} row(s) selected</span>
 			<div class="mt-2 flex sm:mt-0">
-
-        <brn-select class="inline-block" placeholder={{_availablePageSizes[0]}} 
-          [ngModel]="_pageSize()"(ngModelChange)="_pageSize.set($event)">
-          <hlm-select-trigger class="w-15 h-9 mr-1 inline-flex">
-            <hlm-select-value />
-          </hlm-select-trigger>
-          <hlm-select-content>
-            @for (size of _availablePageSizes; track size) {
-						  <hlm-option [value]="size">
-                {{ size === 10000 ? 'All' : size }}
-              </hlm-option>
-					  }
-			    </hlm-select-content>
-        </brn-select>
+				<brn-select
+					class="inline-block"
+					placeholder="{{ _availablePageSizes[0] }}"
+					[ngModel]="_pageSize()"
+					(ngModelChange)="_pageSize.set($event)"
+				>
+					<hlm-select-trigger class="w-15 mr-1 inline-flex h-9">
+						<hlm-select-value />
+					</hlm-select-trigger>
+					<hlm-select-content>
+						@for (size of _availablePageSizes; track size) {
+							<hlm-option [value]="size">
+								{{ size === 10000 ? 'All' : size }}
+							</hlm-option>
+						}
+					</hlm-select-content>
+				</brn-select>
 
 				<div class="flex space-x-1">
 					<button size="sm" variant="outline" hlmBtn [disabled]="!ctx.decrementable()" (click)="ctx.decrement()">
