fbf92e39995d6c8a7ba48c813a1ab9f3bb78a67e
Ben Snyder <ben@snyder.tech>
2024-04-23 14:00:13 -0400

    feat(nx): convert to eslint flat config (#265)
    * feat(nx): convert to eslint flat config
    
    * fix(nx): use .cjs extensions for flat config to work
    
    ---------
    
    Co-authored-by: Robin Goetz <35136007+goetzrobin@users.noreply.github.com>

---
+++ eslint.config.cjs
@@ -0,0 +1,69 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const nxEslintPlugin = require('@nx/eslint-plugin');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+	{ plugins: { '@nx': nxEslintPlugin } },
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		rules: {
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					enforceBuildableLibDependency: true,
+					allow: [],
+					depConstraints: [
+						{
+							sourceTag: '*',
+							onlyDependOnLibsWithTags: ['*'],
+						},
+					],
+				},
+			],
+		},
+	},
+	...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
+		...config,
+		files: ['**/*.ts', '**/*.tsx'],
+		rules: {},
+	})),
+	...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
+		...config,
+		files: ['**/*.js', '**/*.jsx'],
+		rules: {},
+	})),
+	...compat.config({ env: { jest: true } }).map((config) => ({
+		...config,
+		files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+		rules: {},
+	})),
+	...compat.config({ parser: 'jsonc-eslint-parser' }).map((config) => ({
+		...config,
+		files: ['**/*.json'],
+		rules: {
+			'@nx/dependency-checks': [
+				'error',
+				{
+					buildTargets: ['build'],
+					checkMissingDependencies: true,
+					checkObsoleteDependencies: true,
+					checkVersionMismatches: true,
+					ignoredDependencies: [
+						'jest-preset-angular',
+						'jest-axe',
+						'@testing-library/jest-dom',
+						'rxjs',
+						'@spartan-ng/ui-icon-helm',
+						'@spartan-ng/ui-button-helm',
+						'@spartan-ng/ui-avatar-brain',
+					],
+				},
+			],
+		},
+	})),
+];
