84e83e3b16e8b22223e0c580a4851d830bfe2729
Can Serkan UREN <canserkan.uren@gmail.com>
2024-06-21 15:44:08 +0200

    fix(checkbox): update disabled input state when parent form is disabled (#313)
    * fix(checkbox): update disabled input state when parent form is disabled
    
    * fix(checkbox): revert back to input and create internal disabled signal

--- libs/ui/checkbox/checkbox.stories.ts
+++ libs/ui/checkbox/checkbox.stories.ts
@@ -1,9 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
 
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HlmButtonModule } from '../button/helm/src';
 import { HlmIconComponent } from '../icon/helm/src';
 import { HlmLabelDirective } from '../label/helm/src';
 import { HlmCheckboxComponent, HlmCheckboxImports } from './helm/src';
+
+@Component({
+	selector: 'hlm-checkbox-component-tester',
+	template: `
+		<div class="flex items-center gap-4" [formGroup]="form">
+			<label id="checkbox-label" for="testCheckboxDis1" hlmLabel>
+				Test Disabled Checkbox with Reactive Forms
+				<hlm-checkbox class="ml-2" id="testCheckboxDis1" aria-labelledby="testCheckbox" formControlName="checkbox" />
+			</label>
+
+			<button hlmBtn type="button" role="button" (click)="enableOrDisableCheckbox()">Enable or disable button</button>
+		</div>
+	`,
+})
+class HlmCheckboxComponentTester {
+	form = inject(FormBuilder).group({
+		checkbox: [false],
+	});
+
+	enableOrDisableCheckbox(): void {
+		this.form.enabled ? this.form.disable() : this.form.enable();
+	}
+}
 
 const meta: Meta<HlmCheckboxComponent> = {
 	title: 'Checkbox',
@@ -11,7 +37,8 @@ const meta: Meta<HlmCheckboxComponent> = {
 	tags: ['autodocs'],
 	decorators: [
 		moduleMetadata({
-			imports: [HlmCheckboxImports, HlmLabelDirective, HlmIconComponent],
+			declarations: [HlmCheckboxComponentTester],
+			imports: [HlmCheckboxImports, HlmLabelDirective, HlmIconComponent, ReactiveFormsModule, HlmButtonModule],
 		}),
 	],
 };
@@ -22,6 +49,7 @@ type Story = StoryObj<HlmCheckboxComponent>;
 export const Default: Story = {
 	render: () => ({
 		template: `
+		/* HTML */
     <label id='checkbox-label' class='' hlmLabel> Test Checkbox
        <hlm-checkbox id='testCheckbox' aria-checked='mixed' aria-label='test checkbox'/>
     </label>
@@ -32,6 +60,7 @@ export const Default: Story = {
 export const InsideLabel: Story = {
 	render: () => ({
 		template: `
+			/* HTML */
       <label id='checkbox-label' class='flex items-center' hlmLabel> Test Checkbox
         <hlm-checkbox class='ml-2' id='testCheckbox'/>
       </label>
@@ -42,6 +71,7 @@ export const InsideLabel: Story = {
 export const LabeledWithAriaLabeledBy: Story = {
 	render: () => ({
 		template: `
+			/* HTML */
       <div id='checkbox-label' class='flex items-center'>
          <label id='testCheckbox' for='testCheckboxAria' hlmLabel> Test Checkbox </label>
          <hlm-checkbox class='ml-2' id='testCheckboxAria' aria-labelledby='testCheckbox'/>
@@ -49,12 +79,14 @@ export const LabeledWithAriaLabeledBy: Story = {
     `,
 	}),
 };
+
 export const disabled: Story = {
 	render: () => ({
 		template: `
+			/* HTML */
       <div class='flex items-center'>
-         <label id='checkbox-label' for='testCheckboxDis1' hlmLabel> Test Checkbox </label>
-       <hlm-checkbox disabled class='ml-2' id='testCheckboxDis1' aria-labelledby='testCheckbox'/>
+        <label id='checkbox-label' for='testCheckboxDis1' hlmLabel> Test Checkbox </label>
+       	<hlm-checkbox disabled class='ml-2' id='testCheckboxDis1' aria-labelledby='testCheckbox'/>
       </div>
 
       <div class='flex items-center pt-4'>
@@ -63,9 +95,18 @@ export const disabled: Story = {
       </div>
 
       <div class='flex items-center pt-4'>
-      <hlm-checkbox id='testCheckboxDis3' />
-      <label class ="ml-2" for='testCheckboxDis3' hlmLabel> Test Checkbox 3 enabled</label>
-    </div>
+      	<hlm-checkbox id='testCheckboxDis3' />
+      	<label class ="ml-2" for='testCheckboxDis3' hlmLabel> Test Checkbox 3 enabled</label>
+    	</div>
+    `,
+	}),
+};
+
+export const disabledWithForms: Story = {
+	render: () => ({
+		template: `
+			/* HTML */
+			<hlm-checkbox-component-tester />
     `,
 	}),
 };
@@ -73,9 +114,10 @@ export const disabled: Story = {
 export const indeterminate: Story = {
 	render: () => ({
 		template: `
+			/* HTML */
       <div id='checkbox-label' class='flex items-center'>
-         <label id='testCheckbox' for='testCheckboxIndeterminate' hlmLabel> Test Checkbox </label>
-         <hlm-checkbox checked="indeterminate" class='ml-2' id='testCheckboxIndeterminate' aria-labelledby='testCheckbox'/>
+        <label id='testCheckbox' for='testCheckboxIndeterminate' hlmLabel> Test Checkbox </label>
+        <hlm-checkbox checked="indeterminate" class='ml-2' id='testCheckboxIndeterminate' aria-labelledby='testCheckbox'/>
       </div>
     `,
 	}),
