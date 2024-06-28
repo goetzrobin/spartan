4d2c162a383c7bcbfdfed4b33170f6010386acc0
Josue Morales <dev.carlos.morales@gmail.com>
2024-06-28 16:20:36 -0500

    feat(select): implement form-field-control to handle error state in component

--- libs/ui/select/brain/src/lib/brn-select.component.ts
+++ libs/ui/select/brain/src/lib/brn-select.component.ts
@@ -1,37 +1,39 @@
 import { CdkListbox, CdkListboxModule } from '@angular/cdk/listbox';
 import {
 	CdkConnectedOverlay,
+	OverlayModule,
 	type ConnectedOverlayPositionChange,
 	type ConnectedPosition,
-	OverlayModule,
 } from '@angular/cdk/overlay';
 import {
-	type AfterContentInit,
 	ChangeDetectionStrategy,
 	Component,
 	ContentChild,
 	ContentChildren,
+	DoCheck,
 	EventEmitter,
 	Input,
 	Output,
-	type QueryList,
-	type Signal,
 	ViewChild,
 	computed,
 	inject,
 	input,
 	signal,
+	type AfterContentInit,
+	type QueryList,
+	type Signal,
 } from '@angular/core';
 import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { type ControlValueAccessor, NgControl } from '@angular/forms';
+import { FormGroupDirective, NgControl, NgForm, type ControlValueAccessor } from '@angular/forms';
 import {
-	type ExposesSide,
-	type ExposesState,
 	provideExposedSideProviderExisting,
 	provideExposesStateProviderExisting,
+	type ExposesSide,
+	type ExposesState,
 } from '@spartan-ng/ui-core';
+import { ErrorStateMatcher, ErrorStateTracker, HlmFormFieldControl } from '@spartan-ng/ui-form-field-helm';
 import { BrnLabelDirective } from '@spartan-ng/ui-label-brain';
-import { Subject, delay, filter, map, of, switchMap } from 'rxjs';
+import { Subject, delay, map, of, switchMap } from 'rxjs';
 import { BrnSelectContentComponent } from './brn-select-content.component';
 import { BrnSelectOptionDirective } from './brn-select-option.directive';
 import { BrnSelectTriggerDirective } from './brn-select-trigger.directive';
@@ -51,6 +53,10 @@ let nextId = 0;
 		CdkListbox,
 		provideExposedSideProviderExisting(() => BrnSelectComponent),
 		provideExposesStateProviderExisting(() => BrnSelectComponent),
+		{
+			provide: HlmFormFieldControl,
+			useExisting: BrnSelectComponent,
+		},
 	],
 	template: `
 		@if (!labelProvided() && _placeholder()) {
@@ -79,7 +85,9 @@ let nextId = 0;
 		</ng-template>
 	`,
 })
-export class BrnSelectComponent implements ControlValueAccessor, AfterContentInit, ExposesSide, ExposesState {
+export class BrnSelectComponent
+	implements ControlValueAccessor, AfterContentInit, DoCheck, ExposesSide, ExposesState, HlmFormFieldControl
+{
 	private readonly _selectService = inject(BrnSelectService);
 
 	public readonly triggerWidth = this._selectService.triggerWidth;
@@ -189,6 +197,14 @@ export class BrnSelectComponent implements ControlValueAccessor, AfterContentIni
 		},
 	];
 
+	errorStateTracker: ErrorStateTracker;
+
+	private defaultErrorStateMatcher = inject(ErrorStateMatcher);
+	private parentForm = inject(NgForm, { optional: true });
+	private parentFormGroup = inject(FormGroupDirective, { optional: true });
+
+	errorState = computed(() => this.errorStateTracker.errorState());
+
 	constructor() {
 		this._selectService.state.update((state) => ({
 			...state,
@@ -214,16 +230,20 @@ export class BrnSelectComponent implements ControlValueAccessor, AfterContentIni
 		 * ShouldEmitValueChange simply ensures we only propagate value change when a user makes a selection
 		 * we dont propagate changes made from outside the component (ex. patch value or initial value from form control)
 		 */
-		toObservable(this._selectService.value)
-			.subscribe((value) => {
-				if (this._shouldEmitValueChange()) {
-					this._onChange(value ?? null)
-				}
-				this._shouldEmitValueChange.set(true);
-			});
+		toObservable(this._selectService.value).subscribe((value) => {
+			if (this._shouldEmitValueChange()) {
+				this._onChange(value ?? null);
+			}
+			this._shouldEmitValueChange.set(true);
+		});
 
-		toObservable(this.dir).subscribe((dir) =>
-			this._selectService.state.update((state) => ({ ...state, dir })),
+		toObservable(this.dir).subscribe((dir) => this._selectService.state.update((state) => ({ ...state, dir })));
+
+		this.errorStateTracker = new ErrorStateTracker(
+			this.defaultErrorStateMatcher,
+			this.ngControl,
+			this.parentFormGroup,
+			this.parentForm,
 		);
 	}
 
@@ -243,6 +263,10 @@ export class BrnSelectComponent implements ControlValueAccessor, AfterContentIni
 				dir: this.dir(),
 			}));
 		}
+	}
+
+	ngDoCheck() {
+		this.errorStateTracker.updateErrorState();
 	}
 
 	public toggle(): void {
