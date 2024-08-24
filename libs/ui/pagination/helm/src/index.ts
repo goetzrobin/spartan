9496bde27fd94e0970f5623cd10e1164dd7c964a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-08-24 19:25:38 +0800

    fix(pagination): rename componet -> component

--- libs/ui/pagination/helm/src/index.ts
+++ libs/ui/pagination/helm/src/index.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { HlmPaginationContentDirective } from './lib/hlm-pagination-content.directive';
-import { HlmPaginationEllipsisComponent } from './lib/hlm-pagination-ellipsis.componet';
+import { HlmPaginationEllipsisComponent } from './lib/hlm-pagination-ellipsis.component';
 import { HlmPaginationItemDirective } from './lib/hlm-pagination-item.directive';
 import { HlmPaginationLinkDirective } from './lib/hlm-pagination-link.directive';
-import { HlmPaginationNextComponent } from './lib/hlm-pagination-next.componet';
-import { HlmPaginationPreviousComponent } from './lib/hlm-pagination-previous.componet';
+import { HlmPaginationNextComponent } from './lib/hlm-pagination-next.component';
+import { HlmPaginationPreviousComponent } from './lib/hlm-pagination-previous.component';
 import { HlmPaginationDirective } from './lib/hlm-pagination.directive';
 
 export * from './lib/hlm-pagination-content.directive';
-export * from './lib/hlm-pagination-ellipsis.componet';
+export * from './lib/hlm-pagination-ellipsis.component';
 export * from './lib/hlm-pagination-item.directive';
 export * from './lib/hlm-pagination-link.directive';
-export * from './lib/hlm-pagination-next.componet';
-export * from './lib/hlm-pagination-previous.componet';
+export * from './lib/hlm-pagination-next.component';
+export * from './lib/hlm-pagination-previous.component';
 export * from './lib/hlm-pagination.directive';
 
 export const HlmPaginationImports = [
