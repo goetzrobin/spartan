78e0ac2d2bf5362eca914fd2be7d99afe56d885a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-08 13:43:37 +0800

    feat(pagination): add advanced pagination in libs/ui

--- libs/ui/pagination/helm/src/index.ts
+++ libs/ui/pagination/helm/src/index.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { HlmNumberedPaginationComponent } from './lib/hlm-numbered-pagination.component';
 import { HlmPaginationContentDirective } from './lib/hlm-pagination-content.directive';
 import { HlmPaginationEllipsisComponent } from './lib/hlm-pagination-ellipsis.component';
 import { HlmPaginationItemDirective } from './lib/hlm-pagination-item.directive';
@@ -8,6 +9,7 @@ import { HlmPaginationNextComponent } from './lib/hlm-pagination-next.component'
 import { HlmPaginationPreviousComponent } from './lib/hlm-pagination-previous.component';
 import { HlmPaginationDirective } from './lib/hlm-pagination.directive';
 
+export * from './lib/hlm-numbered-pagination.component';
 export * from './lib/hlm-pagination-content.directive';
 export * from './lib/hlm-pagination-ellipsis.component';
 export * from './lib/hlm-pagination-item.directive';
@@ -24,6 +26,7 @@ export const HlmPaginationImports = [
 	HlmPaginationPreviousComponent,
 	HlmPaginationNextComponent,
 	HlmPaginationEllipsisComponent,
+	HlmNumberedPaginationComponent,
 ] as const;
 
 @NgModule({
