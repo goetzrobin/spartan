d2c34ceb68f9be074d3d9bb1b46cdb2c18384ca9
Ben Snyder <ben@snyder.tech>
2024-09-26 17:55:02 -0400

    chore: upgrade to Nx 19.8.x (#387)

--- libs/tools/src/executors/release/update-version/executor.spec.ts
+++ libs/tools/src/executors/release/update-version/executor.spec.ts
@@ -1,26 +1,29 @@
 import * as process from 'node:process';
 import * as replaceJsonProp from 'replace-json-property';
-
 import * as projectHelpers from '../helpers/projects.helpers';
-
 import executor from './executor';
 
+// Mock the entire replace-json-property module
+jest.mock('replace-json-property', () => ({
+	replace: jest.fn(), // Mock the replace function
+}));
+
 describe('ReplaceVersion Executor', () => {
-	it('should replace the version with in the default path if no path was provided', async () => {
+	it('should replace the version within the default path if no path was provided', async () => {
 		const version = '2.0.0';
 		const libName = 'foo';
 		const mockContext = {} as any;
 
-		process.env.VERSION = version;
+		process.env['VERSION'] = version;
 
 		const root = `libs/${libName}`;
 
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		jest.spyOn(replaceJsonProp, 'replace').mockImplementation(() => {});
+		// Mock the getRoot helper
 		jest.spyOn(projectHelpers, 'getRoot').mockReturnValue(root);
 
 		const output = await executor({}, mockContext);
 
+		// Check that the replace function was called correctly
 		expect(replaceJsonProp.replace).toHaveBeenCalledWith(`${root}/package.json`, 'version', version);
 		expect(output.success).toBe(true);
 	});
--- libs/tools/src/executors/release/update-version/executor.ts
+++ libs/tools/src/executors/release/update-version/executor.ts
@@ -8,7 +8,7 @@ import type { ReplaceVersionExecutorSchema } from './schema';
 
 export default async function runExecutor(_options: ReplaceVersionExecutorSchema, context: ExecutorContext) {
 	const sourceRoot = getRoot(context);
-	replace(`${sourceRoot}/package.json`, 'version', process.env.VERSION);
+	replace(`${sourceRoot}/package.json`, 'version', process.env['VERSION']);
 	return {
 		success: true,
 	};
