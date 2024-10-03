c071cad97e9d4e3379985d678474679ee2c475fd
Leonardo Montini <lion.48m@gmail.com>
2024-10-03 18:54:36 +0200

    feat: add config read/write to store components path

---
+++ libs/cli/src/utils/config.ts
@@ -0,0 +1,30 @@
+import { type Tree, readJson } from '@nx/devkit';
+import { prompt } from 'enquirer';
+
+const configPath = 'components.json';
+
+export type Config = {
+	componentsPath: string;
+};
+
+export async function getOrCreateConfig(tree: Tree) {
+	if (tree.exists(configPath)) {
+		return readJson(tree, configPath) as Promise<Config>; // TODO: Parse with zod and handle errors
+	}
+
+	console.log('Configuration file not found, creating a new one...');
+
+	const { componentsPath } = (await prompt({
+		type: 'input',
+		required: true,
+		name: 'componentsPath',
+		message: 'Choose a directory to place your spartan libraries, e.g. libs/ui',
+		initial: 'libs/ui',
+	})) as { componentsPath: string };
+
+	const config = { componentsPath };
+
+	tree.write(configPath, JSON.stringify(config, null, 2));
+
+	return config;
+}
