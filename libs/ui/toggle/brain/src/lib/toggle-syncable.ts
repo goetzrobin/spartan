2880ebc2bb3e42bb47c8da44309ec75fad165465
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2024-11-14 16:54:23 +0000

    refactor(toggle): migrate to signals (#472)
    * refactor(toggle): migrate to signals
    
    * refactor(toggle): migrate to signals

--- libs/ui/toggle/brain/src/lib/toggle-syncable.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import type { BrnToggleDirective } from './brn-toggle.directive';
-
-export const ToggleSyncable = new InjectionToken<ToggleSyncable>('@spartan-ng/ui/brain toggle syncable token');
-
-export interface ToggleSyncable {
-	_syncToggle(toggle: BrnToggleDirective, state: 'on' | 'off', isUserInput?: boolean, deferEvents?: boolean): void;
-}
