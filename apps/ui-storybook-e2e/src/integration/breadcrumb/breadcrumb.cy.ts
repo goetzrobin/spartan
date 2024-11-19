29658637ad232a87d5a690002708354617eb393b
dongphuong0905 <phuongtransmile99@gmail.com>
2024-11-19 14:37:53 +0700

    feat(breadcrumb): fix e2e test

--- apps/ui-storybook-e2e/src/integration/breadcrumb/breadcrumb.cy.ts
+++ apps/ui-storybook-e2e/src/integration/breadcrumb/breadcrumb.cy.ts
@@ -1,17 +1,17 @@
-describe('Breadcrumb Component', () => {
+describe('Breadcrumb Directive', () => {
 	beforeEach(() => {
 		cy.visit('/iframe.html?id=breadcrumb--default');
 	});
 
-	it('should render the breadcrumb component', () => {
-		cy.get('hlm-breadcrumb').should('exist');
+	it('should render the breadcrumb directive', () => {
+		cy.get('[hlmBreadcrumb]').should('exist');
 	});
 
 	it('should contain the correct breadcrumb items', () => {
-		cy.get('hlm-breadcrumb ol li').should('have.length', 7);
-		cy.get('hlm-breadcrumb ol li').eq(0).contains('Home');
-		cy.get('hlm-breadcrumb ol li').eq(4).contains('Components');
-		cy.get('hlm-breadcrumb ol li').eq(6).contains('Breadcrumb');
+		cy.get('[hlmBreadcrumb] ol li').should('have.length', 7);
+		cy.get('[hlmBreadcrumb] ol li').eq(0).contains('Home');
+		cy.get('[hlmBreadcrumb] ol li').eq(4).contains('Components');
+		cy.get('[hlmBreadcrumb] ol li').eq(6).contains('Breadcrumb');
 	});
 
 	it('should contain breadcrumb separators', () => {
