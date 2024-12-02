661c287037ae4c22520cee2022ee95223154002e
robingotz <tug29225@temple.edu>
2024-12-02 16:07:10 +0100

    build: add executor to replace version for ui libs
    this executor finds all package.json in the libs/ui directory
    and replaces the current version
    of @spartan-ng with the one defined in UI_VERSION

--- libs/tools/src/executors/release/build-update-publish/executor.ts
+++ libs/tools/src/executors/release/build-update-publish/executor.ts
@@ -3,14 +3,14 @@ import { execSync } from 'node:child_process';
 
 import { getProjectName } from '../helpers/projects.helpers';
 import npmPublish from '../npm-publish/executor';
-import updateVersion from '../update-version/executor';
 
 import type { BuildUpdatePublishExecutorSchema } from './schema';
 
 export default async function runExecutor(_options: BuildUpdatePublishExecutorSchema, context: ExecutorContext) {
-	await updateVersion({}, context);
 	execSync(`nx build --project ${getProjectName(context)}`);
+
 	await npmPublish({}, context);
+
 	return {
 		success: true,
 	};
