9f4e9d07477ec59f8af3e38f9b396ff53db83a6c
Merlin Moos <merlin.moos@hotmail.de>
2024-12-02 13:00:58 +0100

    refactor(tooltip): migrate to signals #430

--- libs/ui/tooltip/helm/src/lib/hlm-tooltip-trigger.directive.ts
+++ libs/ui/tooltip/helm/src/lib/hlm-tooltip-trigger.directive.ts
@@ -25,20 +25,23 @@ export class HlmTooltipTriggerDirective {
 
 	constructor() {
 		if (this._brnTooltipTrigger) {
-			this._brnTooltipTrigger.exitAnimationDuration = 150;
-			this._brnTooltipTrigger.hideDelay = 300;
-			this._brnTooltipTrigger.showDelay = 150;
-			this._brnTooltipTrigger.tooltipContentClasses =
-				'overflow-hidden rounded-md border border-border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md fade-in-0 zoom-in-95 ' +
-				'data-[state=open]:animate-in ' +
-				'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 ' +
-				'data-[side=below]:slide-in-from-top-2 data-[side=above]:slide-in-from-bottom-2 ' +
-				'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 ';
+			this._brnTooltipTrigger.exitAnimationDurationState().set(150);
+			this._brnTooltipTrigger.hideDelayState().set(300);
+			this._brnTooltipTrigger.showDelayState().set(150);
+			this._brnTooltipTrigger
+				.tooltipContentClassesState()
+				.set(
+					'overflow-hidden rounded-md border border-border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md fade-in-0 zoom-in-95 ' +
+						'data-[state=open]:animate-in ' +
+						'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 ' +
+						'data-[side=below]:slide-in-from-top-2 data-[side=above]:slide-in-from-bottom-2 ' +
+						'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 ',
+				);
 		}
 	}
 
 	@Input()
 	public set hlmTooltipTrigger(value: string | TemplateRef<unknown> | null) {
-		this._brnTooltipTrigger.content = value;
+		this._brnTooltipTrigger.contentState().set(value);
 	}
 }
