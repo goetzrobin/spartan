af031e927ab6be876df6715d9ebd58032aadbee2
robingotz <tug29225@temple.edu>
2024-12-10 21:44:04 +0100

    fix: rename release scripts and interface and add missing generators

--- libs/cli/src/generators/ui/generator.ts
+++ libs/cli/src/generators/ui/generator.ts
@@ -10,7 +10,9 @@ export default async function hlmUIGenerator(tree: Tree, options: HlmUIGenerator
 	const config = await getOrCreateConfig(tree, {
 		componentsPath: options.directory,
 	});
-	const availablePrimitives: ComponentDefintions = await import('./supported-ui-libraries.json').then((m) => m.default);
+	const availablePrimitives: PrimitiveDefinitions = await import('./supported-ui-libraries.json').then(
+		(m) => m.default,
+	);
 	const availablePrimitiveNames = [...Object.keys(availablePrimitives), 'collapsible', 'menubar', 'contextmenu'];
 	let response: { primitives: string[] } = { primitives: [] };
 	if (options.name && availablePrimitiveNames.includes(options.name)) {
@@ -36,7 +38,7 @@ async function createPrimitiveLibraries(
 		primitives: string[];
 	},
 	availablePrimitiveNames: string[],
-	availablePrimitives: ComponentDefintions,
+	availablePrimitives: PrimitiveDefinitions,
 	tree: Tree,
 	options: HlmUIGeneratorSchema & { angularCli?: boolean },
 	config: Config,
@@ -118,7 +120,7 @@ const replaceContextAndMenuBar = async (primtivesToCreate: string[], silent = fa
 const removeHelmKeys = (obj: Record<string, string>) =>
 	Object.fromEntries(Object.entries(obj).filter(([key]) => !key.toLowerCase().includes('helm')));
 
-interface ComponentDefintions {
+interface PrimitiveDefinitions {
 	[componentName: string]: {
 		internalName: string;
 		peerDependencies: Record<string, string>;
