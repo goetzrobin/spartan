941f835b2c6a2ec01f8dfbe4fe39178d7c3abd08
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-12-12 14:37:54 +0100

    refactor(radio-group): change brn-radio-group to directive and hlm-radio-group to component
    * expose brn-radio-group inputs/outputs
    * apply dir and tabindex attribute for radio-group
    * change styles for radio-group
    * apply reverse style for rtl direction

--- libs/ui/radio-group/helm/src/index.ts
+++ libs/ui/radio-group/helm/src/index.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { HlmRadioGroupDirective } from './lib/hlm-radio-group.directive';
+import { HlmRadioGroupComponent } from './lib/hlm-radio-group.component';
 import { HlmRadioIndicatorComponent } from './lib/hlm-radio-indicator.component';
 import { HlmRadioDirective } from './lib/hlm-radio.directive';
 
-export * from './lib/hlm-radio-group.directive';
+export * from './lib/hlm-radio-group.component';
 export * from './lib/hlm-radio-indicator.component';
 export * from './lib/hlm-radio.directive';
 
-export const HlmRadioGroupImports = [HlmRadioGroupDirective, HlmRadioDirective, HlmRadioIndicatorComponent];
+export const HlmRadioGroupImports = [HlmRadioGroupComponent, HlmRadioDirective, HlmRadioIndicatorComponent];
 
 @NgModule({
 	imports: [...HlmRadioGroupImports],
