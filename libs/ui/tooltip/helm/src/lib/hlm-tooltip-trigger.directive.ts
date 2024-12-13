18e4cfb8a5ac1f2e4a792781ca3c6408e6dacf7c
moos <merlin.moos@oldendorff.com>
2024-12-13 15:02:24 +0100

    refactor(tooltip): change naming

--- libs/ui/tooltip/helm/src/lib/hlm-tooltip-trigger.directive.ts
+++ libs/ui/tooltip/helm/src/lib/hlm-tooltip-trigger.directive.ts
@@ -1,5 +1,5 @@
-import {Directive, inject, Input, type TemplateRef} from '@angular/core';
-import {BrnTooltipTriggerDirective} from '@spartan-ng/brain/tooltip';
+import { Directive, inject, Input, type TemplateRef } from '@angular/core';
+import { BrnTooltipTriggerDirective } from '@spartan-ng/brain/tooltip';
 
 @Directive({
 	selector: '[hlmTooltipTrigger]',
@@ -21,7 +21,7 @@ import {BrnTooltipTriggerDirective} from '@spartan-ng/brain/tooltip';
 	],
 })
 export class HlmTooltipTriggerDirective {
-	private readonly _brnTooltipTrigger: BrnTooltipTriggerDirective = inject(BrnTooltipTriggerDirective, {host: true});
+	private readonly _brnTooltipTrigger: BrnTooltipTriggerDirective = inject(BrnTooltipTriggerDirective, { host: true });
 
 	constructor() {
 		if (this._brnTooltipTrigger) {
@@ -32,10 +32,10 @@ export class HlmTooltipTriggerDirective {
 				.mutableTooltipContentClasses()
 				.set(
 					'overflow-hidden rounded-md border border-border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md fade-in-0 zoom-in-95 ' +
-					'data-[state=open]:animate-in ' +
-					'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 ' +
-					'data-[side=below]:slide-in-from-top-2 data-[side=above]:slide-in-from-bottom-2 ' +
-					'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 ',
+						'data-[state=open]:animate-in ' +
+						'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 ' +
+						'data-[side=below]:slide-in-from-top-2 data-[side=above]:slide-in-from-bottom-2 ' +
+						'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 ',
 				);
 		}
 	}
